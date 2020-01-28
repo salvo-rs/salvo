c11c1599284e5fb94e094b3de05c41275f56dcdc
Chrislearn Young <chris@acroidea.com>
2020-01-28 11:51:08 +0800

    code update

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -5,20 +5,18 @@ use quote::quote;
 
 #[proc_macro_attribute]
 pub fn fn_handler(_: TokenStream, input: TokenStream) -> TokenStream {
-    let mut input = syn::parse_macro_input!(input as syn::ItemFn);
+    let input = syn::parse_macro_input!(input as syn::ItemFn);
     let attrs = &input.attrs;
     let vis = &input.vis;
-    let sig = &mut input.sig;
+    let sig = &input.sig;
     let body = &input.block;
     let name = &sig.ident;
 
-    // if sig.asyncness.is_none() {
-    //     return syn::Error::new_spanned(sig.fn_token, "only async fn is supported")
-    //         .to_compile_error()
-    //         .into();
-    // }
-
-    // sig.asyncness = None;
+    if sig.asyncness.is_none() {
+        return syn::Error::new_spanned(sig.fn_token, "only async fn is supported")
+            .to_compile_error()
+            .into();
+    }
 
     (quote! {
         #vis struct #name;
@@ -31,7 +29,7 @@ pub fn fn_handler(_: TokenStream, input: TokenStream) -> TokenStream {
         #[async_trait]
         impl novel::Handler for #name {
             async fn handle(&self, sconf: Arc<ServerConfig>, req: &mut Request, depot: &mut Depot, resp: &mut Response) {
-                async move {Self::#name(sconf, req, depot, resp).await};
+                Self::#name(sconf, req, depot, resp).await
             }
         }
     }).into()
