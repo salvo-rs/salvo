48db340d6c2ec7f6e9094994db56bf2684bf0eb7
Chrislearn Young <chris@acroidea.com>
2020-02-16 18:34:49 +0800

    prelude logger

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -46,7 +46,7 @@ pub mod prelude {
     pub use crate::depot::Depot;
     pub use crate::http::{Request, Response};
     pub use crate::Handler;
-    pub use crate::logging;
+    pub use crate::logging::{self, logger};
     pub use std::sync::Arc;
     pub use salvo_macros::fn_handler;
     pub use async_trait::async_trait;
