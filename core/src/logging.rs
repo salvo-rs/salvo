822f75ae0f5a54b6b87df1e54f4eb99c4a19c5ab
Chrislearn Young <chris@acroidea.com>
2020-02-25 09:12:50 +0800

    async log

--- core/src/logging.rs
+++ core/src/logging.rs
@@ -1,11 +1,11 @@
-use std::sync::Mutex;
 use slog::{Logger, Drain};
 
 lazy_static! {
     static ref LOGGER: Logger = {
          let decorator = slog_term::TermDecorator::new().build();
-        let drain = Mutex::new(slog_term::FullFormat::new(decorator).build()).fuse();
-        Logger::root(drain, o!("version" => env!("CARGO_PKG_VERSION")))
+        let drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let drain = slog_async::Async::new(drain).build().fuse();
+        Logger::root(drain, o!())
     };
 }
 pub fn logger() -> &'static Logger {
