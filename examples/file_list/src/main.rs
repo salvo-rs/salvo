e6a09389e953a266c19f61fdfcf0440505b21c77
chrislearn <chris@acroidea.com>
2021-01-08 11:24:03 +0800

    WIP

--- examples/file_list/src/main.rs
+++ examples/file_list/src/main.rs
@@ -4,9 +4,7 @@ use salvo_extra::serve::Static;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let mut router = Router::new("/<*path>");
-    router.get(Static::from("./static"));
-    println!("{:#?}", &router);
+    let router = Router::new().path("files/<*path>").get(Static::from("./static"));
     let server = Server::with_addr(router, "127.0.0.1:9688");
     server.serve().await?;
     Ok(())
