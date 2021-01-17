1ea96709a882139b58468e0677db16be07e1d373
chrislearn <chris@acroidea.com>
2021-01-17 08:35:31 +0800

    tls supports

--- examples/file_list.rs
+++ examples/file_list.rs
@@ -5,7 +5,6 @@ use salvo_extra::serve::Static;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let router = Router::new().path("files/<*path>").get(Static::from("./static"));
-    let server = Server::with_addr(router, "127.0.0.1:9688");
-    server.serve().await?;
+    Server::new(router).run(([127, 0, 0, 1], 9688)).await;
     Ok(())
 }
