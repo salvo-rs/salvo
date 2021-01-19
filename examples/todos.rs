35ca9a81de04f7efb729a4a65ed369be10102ca3
chrislearn <chris@acroidea.com>
2021-01-19 21:54:13 +0800

    Update todos.rs
    Former-commit-id: 26b1cadcaa46c0d46c858a1fee0be548d2a44b77

--- examples/todos.rs
+++ examples/todos.rs
@@ -1,7 +1,13 @@
-#![deny(warnings)]
+use once_cell::sync::Lazy;
+use tracing;
+use tracing_subscriber;
+use tracing_subscriber::fmt::format::FmtSpan;
 
-use std::env;
-use salvo::Filter;
+use salvo::prelude::*;
+
+use self::models::*;
+
+static DB: Lazy<Db> = Lazy::new(|| blank_db());
 
 /// Provides a RESTful web server managing some Todos.
 ///
@@ -13,183 +19,98 @@ use salvo::Filter;
 /// - `DELETE /todos/:id`: delete a specific Todo.
 #[tokio::main]
 async fn main() {
-    if env::var_os("RUST_LOG").is_none() {
-        // Set `RUST_LOG=todos=debug` to see debug logs,
-        // this only shows access logs.
-        env::set_var("RUST_LOG", "todos=info");
-    }
-    let db = models::blank_db();
-
-    let api = filters::todos(db);
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "todos=debug,salvo=debug".to_owned());
+    tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
 
     // View access logs by setting `RUST_LOG=todos`.
-    let routes = api.with(salvo::log("todos"));
+    let router = Router::new().path("todos").get(list_todos).post(create_todo).push(
+        Router::new().path("<id>").put(update_todo).delete(delete_todo)
+    );
     // Start up the server...
-    salvo::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    Server::new(router).bind(([127, 0, 0, 1], 3040)).await;
 }
 
-mod filters {
-    use super::handlers;
-    use super::models::{Db, ListOptions, Todo};
-    use salvo::Filter;
-
-    /// The 4 TODOs filters combined.
-    pub fn todos(
-        db: Db,
-    ) -> impl Filter<Extract = impl salvo::Reply, Error = salvo::Rejection> + Clone {
-        todos_list(db.clone())
-            .or(todos_create(db.clone()))
-            .or(todos_update(db.clone()))
-            .or(todos_delete(db))
-    }
-
-    /// GET /todos?offset=3&limit=5
-    pub fn todos_list(
-        db: Db,
-    ) -> impl Filter<Extract = impl salvo::Reply, Error = salvo::Rejection> + Clone {
-        salvo::path!("todos")
-            .and(salvo::get())
-            .and(salvo::query::<ListOptions>())
-            .and(with_db(db))
-            .and_then(handlers::list_todos)
-    }
-
-    /// POST /todos with JSON body
-    pub fn todos_create(
-        db: Db,
-    ) -> impl Filter<Extract = impl salvo::Reply, Error = salvo::Rejection> + Clone {
-        salvo::path!("todos")
-            .and(salvo::post())
-            .and(json_body())
-            .and(with_db(db))
-            .and_then(handlers::create_todo)
-    }
-
-    /// PUT /todos/:id with JSON body
-    pub fn todos_update(
-        db: Db,
-    ) -> impl Filter<Extract = impl salvo::Reply, Error = salvo::Rejection> + Clone {
-        salvo::path!("todos" / u64)
-            .and(salvo::put())
-            .and(json_body())
-            .and(with_db(db))
-            .and_then(handlers::update_todo)
-    }
-
-    /// DELETE /todos/:id
-    pub fn todos_delete(
-        db: Db,
-    ) -> impl Filter<Extract = impl salvo::Reply, Error = salvo::Rejection> + Clone {
-        // We'll make one of our endpoints admin-only to show how authentication filters are used
-        let admin_only = salvo::header::exact("authorization", "Bearer admin");
-
-        salvo::path!("todos" / u64)
-            // It is important to put the auth check _after_ the path filters.
-            // If we put the auth check before, the request `PUT /todos/invalid-string`
-            // would try this filter and reject because the authorization header doesn't match,
-            // rather because the param is wrong for that other path.
-            .and(admin_only)
-            .and(salvo::delete())
-            .and(with_db(db))
-            .and_then(handlers::delete_todo)
-    }
-
-    fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
-        salvo::any().map(move || db.clone())
-    }
-
-    fn json_body() -> impl Filter<Extract = (Todo,), Error = salvo::Rejection> + Clone {
-        // When accepting a body, we want a JSON body
-        // (and to reject huge payloads)...
-        salvo::body::content_length_limit(1024 * 16).and(salvo::body::json())
-    }
+#[fn_handler]
+pub async fn list_todos(req: &mut Request, res: &mut Response) {
+    let opts = req.read::<ListOptions>().await.unwrap();
+    // Just return a JSON array of todos, applying the limit and offset.
+    let todos = DB.lock().await;
+    let todos: Vec<Todo> = todos
+        .clone()
+        .into_iter()
+        .skip(opts.offset.unwrap_or(0))
+        .take(opts.limit.unwrap_or(std::usize::MAX))
+        .collect();
+    res.render_json(&todos);
 }
 
-/// These are our API handlers, the ends of each filter chain.
-/// Notice how thanks to using `Filter::and`, we can define a function
-/// with the exact arguments we'd expect from each filter in the chain.
-/// No tuples are needed, it's auto flattened for the functions.
-mod handlers {
-    use super::models::{Db, ListOptions, Todo};
-    use std::convert::Infallible;
-    use salvo::http::StatusCode;
+#[fn_handler]
+pub async fn create_todo(req: &mut Request, res: &mut Response) {
+    let create = req.read::<Todo>().await.unwrap();
+    tracing::debug!("create_todo: {:?}", create);
 
-    pub async fn list_todos(opts: ListOptions, db: Db) -> Result<impl salvo::Reply, Infallible> {
-        // Just return a JSON array of todos, applying the limit and offset.
-        let todos = db.lock().await;
-        let todos: Vec<Todo> = todos
-            .clone()
-            .into_iter()
-            .skip(opts.offset.unwrap_or(0))
-            .take(opts.limit.unwrap_or(std::usize::MAX))
-            .collect();
-        Ok(salvo::reply::json(&todos))
+    let mut vec = DB.lock().await;
+
+    for todo in vec.iter() {
+        if todo.id == create.id {
+            tracing::debug!("    -> id already exists: {}", create.id);
+            // Todo with id already exists, return `400 BadRequest`.
+            res.set_status_code(StatusCode::BAD_REQUEST);
+            return;
+        }
     }
 
-    pub async fn create_todo(create: Todo, db: Db) -> Result<impl salvo::Reply, Infallible> {
-        log::debug!("create_todo: {:?}", create);
+    // No existing Todo with id, so insert and return `201 Created`.
+    vec.push(create);
+    res.set_status_code(StatusCode::CREATED);
+}
 
-        let mut vec = db.lock().await;
+#[fn_handler]
+pub async fn update_todo(req: &mut Request, res: &mut Response) {
+    let id = req.get_param::<u64>("id").unwrap();
+    let update = req.read::<Todo>().await.unwrap();
+    tracing::debug!("update_todo: id={}, todo={:?}", id, update);
+    let mut vec = DB.lock().await;
 
-        for todo in vec.iter() {
-            if todo.id == create.id {
-                log::debug!("    -> id already exists: {}", create.id);
-                // Todo with id already exists, return `400 BadRequest`.
-                return Ok(StatusCode::BAD_REQUEST);
-            }
+    // Look for the specified Todo...
+    for todo in vec.iter_mut() {
+        if todo.id == id {
+            *todo = update;
+            res.set_status_code(StatusCode::OK);
+            return;
         }
-
-        // No existing Todo with id, so insert and return `201 Created`.
-        vec.push(create);
-
-        Ok(StatusCode::CREATED)
     }
 
-    pub async fn update_todo(
-        id: u64,
-        update: Todo,
-        db: Db,
-    ) -> Result<impl salvo::Reply, Infallible> {
-        log::debug!("update_todo: id={}, todo={:?}", id, update);
-        let mut vec = db.lock().await;
+    tracing::debug!("    -> todo id not found!");
 
-        // Look for the specified Todo...
-        for todo in vec.iter_mut() {
-            if todo.id == id {
-                *todo = update;
-                return Ok(StatusCode::OK);
-            }
-        }
+    // If the for loop didn't return OK, then the ID doesn't exist...
+    res.set_status_code(StatusCode::NOT_FOUND);
+}
 
-        log::debug!("    -> todo id not found!");
+#[fn_handler]
+pub async fn delete_todo(req: &mut Request, res: &mut Response) {
+    let id = req.get_param::<u64>("id").unwrap();
+    tracing::debug!("delete_todo: id={}", id);
 
-        // If the for loop didn't return OK, then the ID doesn't exist...
-        Ok(StatusCode::NOT_FOUND)
-    }
+    let mut vec = DB.lock().await;
 
-    pub async fn delete_todo(id: u64, db: Db) -> Result<impl salvo::Reply, Infallible> {
-        log::debug!("delete_todo: id={}", id);
+    let len = vec.len();
+    vec.retain(|todo| {
+        // Retain all Todos that aren't this id...
+        // In other words, remove all that *are* this id...
+        todo.id != id
+    });
 
-        let mut vec = db.lock().await;
+    // If the vec is smaller, we found and deleted a Todo!
+    let deleted = vec.len() != len;
 
-        let len = vec.len();
-        vec.retain(|todo| {
-            // Retain all Todos that aren't this id...
-            // In other words, remove all that *are* this id...
-            todo.id != id
-        });
-
-        // If the vec is smaller, we found and deleted a Todo!
-        let deleted = vec.len() != len;
-
-        if deleted {
-            // respond with a `204 No Content`, which means successful,
-            // yet no body expected...
-            Ok(StatusCode::NO_CONTENT)
-        } else {
-            log::debug!("    -> todo id not found!");
-            Ok(StatusCode::NOT_FOUND)
-        }
+    if deleted {
+        // respond with a `204 No Content`, which means successful,
+        // yet no body expected...
+        res.set_status_code(StatusCode::NO_CONTENT);
+    } else {
+        tracing::debug!("    -> todo id not found!");
+        res.set_status_code(StatusCode::NOT_FOUND);
     }
 }
 
@@ -224,7 +145,7 @@ mod models {
 #[cfg(test)]
 mod tests {
     use salvo::http::StatusCode;
-    use salvo::test::request;
+    use reqwest::Client;
 
     use super::{
         filters,
@@ -233,54 +154,32 @@ mod tests {
 
     #[tokio::test]
     async fn test_post() {
-        let db = models::blank_db();
-        let api = filters::todos(db);
-
-        let resp = request()
-            .method("POST")
-            .path("/todos")
+        let client = Client::new();
+        let resp = client.post("https://127.0.0.1:3030/todos")
             .json(&Todo {
                 id: 1,
                 text: "test 1".into(),
                 completed: false,
             })
-            .reply(&api)
-            .await;
+            .send()
+            .await?;
 
         assert_eq!(resp.status(), StatusCode::CREATED);
     }
 
     #[tokio::test]
     async fn test_post_conflict() {
-        let db = models::blank_db();
-        db.lock().await.push(todo1());
-        let api = filters::todos(db);
-
-        let resp = request()
-            .method("POST")
-            .path("/todos")
-            .json(&todo1())
-            .reply(&api)
-            .await;
+        let client = Client::new();
+        let resp = client.post("https://127.0.0.1:3030/todos")
+            .json(&Todo {
+                id: 1,
+                text: "test 1".into(),
+                completed: false,
+            })
+            .send()
+            .await?;
 
         assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
-    }
-
-    #[tokio::test]
-    async fn test_put_unknown() {
-        let _ = pretty_env_logger::try_init();
-        let db = models::blank_db();
-        let api = filters::todos(db);
-
-        let resp = request()
-            .method("PUT")
-            .path("/todos/1")
-            .header("authorization", "Bearer admin")
-            .json(&todo1())
-            .reply(&api)
-            .await;
-
-        assert_eq!(resp.status(), StatusCode::NOT_FOUND);
     }
 
     fn todo1() -> Todo {
