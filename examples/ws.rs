fe6f3a2579d006fb34d98394647618bb8287e875
chrislearn <chris@acroidea.com>
2021-01-20 08:19:40 +0800

    update ws api

--- examples/ws.rs
+++ examples/ws.rs
@@ -2,21 +2,32 @@ use futures::{FutureExt, StreamExt};
 use tokio;
 
 use salvo::prelude::*;
-use salvo_extra::ws::{WebSocket, WsHandler};
+use salvo_extra::ws::WsHandler;
 
-fn callback(_req: Request, _depot: Depot, ws: WebSocket) {
-    // Just echo all messages back...
-    let (tx, rx) = ws.split();
-    let fut = rx.forward(tx).map(|result| {
-        if let Err(e) = result {
-            eprintln!("websocket error: {:?}", e);
+#[fn_handler]
+async fn connect(req: &mut Request, res: &mut Response) -> Result<(), HttpError> {
+    match WsHandler::new().handle(req, res){
+        Ok(fut) => {
+            let fut = async move {
+                if let Some(ws) = fut.await {
+                    let (tx, rx) = ws.split();
+                    let fut = rx.forward(tx).map(|result| {
+                        if let Err(e) = result {
+                            eprintln!("websocket error: {:?}", e);
+                        }
+                    });
+                    tokio::task::spawn(fut);
+                }
+            };
+            tokio::task::spawn(fut);
+            Ok(())
         }
-    });
-    tokio::task::spawn(fut);
+        Err(e) => Err(e)
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().handle(WsHandler::new(callback));
+    let router = Router::new().handle(connect);
     Server::new(router).bind(([127, 0, 0, 1], 7878)).await;
 }
--- examples/ws_chat.rs
+++ examples/ws_chat.rs
@@ -12,7 +12,7 @@ use tracing_subscriber;
 use tracing_subscriber::fmt::format::FmtSpan;
 
 use salvo::prelude::*;
-use salvo_extra::ws::{Message, WebSocket, WsHandler};
+use salvo_extra::ws::{Message, WsHandler};
 
 /// Our global unique user id counter.
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
@@ -31,53 +31,63 @@ static GLOBAL_USERS: Lazy<Users> = Lazy::new(|| Users::default());
 async fn main() {
     let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "ws_chat=debug,salvo=debug".to_owned());
     tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
-    let router = Router::new()
-        .handle(index)
-        .push(Router::new().path("chat").handle(WsHandler::new(user_connected)));
+    let router = Router::new().handle(index).push(Router::new().path("chat").handle(user_connected));
     Server::new(router).bind(([127, 0, 0, 1], 3131)).await;
 }
 
-fn user_connected(_req: Request, _depot: Depot, ws: WebSocket) {
-    // Use a counter to assign a new unique ID for this user.
-    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+#[fn_handler]
+async fn user_connected(req: &mut Request, res: &mut Response) -> Result<(), HttpError> {
+    match WsHandler::new().handle(req, res) {
+        Ok(fut) => {
+            let fut = async move {
+                if let Some(ws) = fut.await {
+                    // Use a counter to assign a new unique ID for this user.
+                    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-    eprintln!("new chat user: {}", my_id);
+                    eprintln!("new chat user: {}", my_id);
 
-    // Split the socket into a sender and receive of messages.
-    let (user_ws_tx, mut user_ws_rx) = ws.split();
+                    // Split the socket into a sender and receive of messages.
+                    let (user_ws_tx, mut user_ws_rx) = ws.split();
 
-    // Use an unbounded channel to handle buffering and flushing of messages
-    // to the websocket...
-    let (tx, rx) = mpsc::unbounded_channel();
-    let rx = UnboundedReceiverStream::new(rx);
-    let fut = rx.forward(user_ws_tx).map(|result| {
-        if let Err(e) = result {
-            eprintln!("websocket send error: {}", e);
-        }
-    });
-    tokio::task::spawn(fut);
-    let fut = async move {
-        // Save the sender in our list of connected users.
-        GLOBAL_USERS.write().await.insert(my_id, tx);
+                    // Use an unbounded channel to handle buffering and flushing of messages
+                    // to the websocket...
+                    let (tx, rx) = mpsc::unbounded_channel();
+                    let rx = UnboundedReceiverStream::new(rx);
+                    let fut = rx.forward(user_ws_tx).map(|result| {
+                        if let Err(e) = result {
+                            eprintln!("websocket send error: {}", e);
+                        }
+                    });
+                    tokio::task::spawn(fut);
+                    let fut = async move {
+                        // Save the sender in our list of connected users.
+                        GLOBAL_USERS.write().await.insert(my_id, tx);
 
-        // Every time the user sends a message, broadcast it to
-        // all other users...
-        while let Some(result) = user_ws_rx.next().await {
-            let msg = match result {
-                Ok(msg) => msg,
-                Err(e) => {
-                    eprintln!("websocket error(uid={}): {}", my_id, e);
-                    break;
+                        // Every time the user sends a message, broadcast it to
+                        // all other users...
+                        while let Some(result) = user_ws_rx.next().await {
+                            let msg = match result {
+                                Ok(msg) => msg,
+                                Err(e) => {
+                                    eprintln!("websocket error(uid={}): {}", my_id, e);
+                                    break;
+                                }
+                            };
+                            user_message(my_id, msg).await;
+                        }
+
+                        // user_ws_rx stream will keep processing as long as the user stays
+                        // connected. Once they disconnect, then...
+                        user_disconnected(my_id).await;
+                    };
+                    tokio::task::spawn(fut);
                 }
             };
-            user_message(my_id, msg).await;
+            tokio::task::spawn(fut);
+            Ok(())
         }
-
-        // user_ws_rx stream will keep processing as long as the user stays
-        // connected. Once they disconnect, then...
-        user_disconnected(my_id).await;
-    };
-    tokio::task::spawn(fut);
+        Err(e) => Err(e),
+    }
 }
 async fn user_message(my_id: usize, msg: Message) {
     // Skip any non-Text messages...
