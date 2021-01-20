3523e40e928d4c70306dbbab16f374fd31c12d9d
chrislearn <chris@acroidea.com>
2021-01-20 22:49:34 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,6 @@
 pub use salvo_core as core;
 pub use salvo_core::*;
 
+
 #[cfg(feature = "extra")]
 pub use salvo_extra as extra;
-
-pub mod prelude {
-    pub use crate::depot::Depot;
-    pub use crate::http::{Request, Response, StatusCode, HttpError};
-    pub use crate::routing::filter;
-    pub use crate::routing::Router;
-    pub use crate::server::Server;
-    pub use crate::writer::*;
-    pub use crate::Handler;
-    pub use async_trait::async_trait;
-    #[cfg(feature = "macros")]
-    pub use salvo_macros::fn_handler;
-}
