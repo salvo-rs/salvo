dcaaf9302af126c8b871e2341cf78b40c2caa2fc
chrislearn <chris@acroidea.com>
2021-01-20 14:15:13 +0800

    Update hello_world.rs

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -19,8 +19,8 @@ async fn hello_world3(_req: &mut Request, _depot: &mut Depot, res: &mut Response
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "hello_world=debug,salvo=debug".to_owned());
     tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
-    let router = Router::new().get(hello_world1);
-    let router = router
+    let router = Router::new()
+        .get(hello_world1)
         .push(Router::new().path("hello2").get(hello_world2))
         .push(Router::new().path("hello3").get(hello_world3));
     Server::new(router).bind(([127, 0, 0, 1], 7878)).await;
