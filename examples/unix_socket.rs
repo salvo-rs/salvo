f7c74906fb458f6b1c68e69b1d71fb99029e8349
chrislearn <chris@acroidea.com>
2021-01-20 14:37:38 +0800

    Update unix_socket.rs
    Former-commit-id: aafd2bf1c94e5ab817d8a135eb8043b9b70f2d9d

--- examples/unix_socket.rs
+++ examples/unix_socket.rs
@@ -1,15 +1,23 @@
-use tokio::net::UnixListener;
-use tokio_stream::wrappers::UnixListenerStream;
-
-use salvo_core::routing::Router;
-use salvo_core::Server;
-use salvo_extra::serve::Static;
-
+#[cfg(target_os = "linux")]
 #[tokio::main]
 async fn main() {
+    #[cfg(target_os = "linux")]
+    use tokio::net::UnixListener;
+    #[cfg(target_os = "linux")]
+    use tokio_stream::wrappers::UnixListenerStream;
+
+    use salvo_core::routing::Router;
+    use salvo_core::Server;
+    use salvo_extra::serve::Static;
+
     let listener = UnixListener::bind("/tmp/salvo.sock").unwrap();
     let incoming = UnixListenerStream::new(listener);
     let router = Router::new().path("files/<*path>").get(Static::from("./static"));
-    Server::new(router).bind_incoming(incoming)
-        .await;
+    Server::new(router).bind_incoming(incoming).await;
+}
+
+#[cfg(not(target_os = "linux"))]
+#[tokio::main]
+async fn main() {
+    println!("please run this example in linux");
 }
