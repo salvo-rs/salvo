e60563efa3bdbc580d246694562ea0a543bed783
chrislearn <chris@acroidea.com>
2021-01-22 20:29:10 +0800

    Update unix_socket.rs
    Former-commit-id: 0a09aa0ae46899c9a3b242ed25433db81bdadcd6

--- examples/unix_socket.rs
+++ examples/unix_socket.rs
@@ -8,11 +8,11 @@ async fn main() {
 
     use salvo_core::routing::Router;
     use salvo_core::Server;
-    use salvo_extra::serve::Static;
+    use salvo_extra::serve::StaticDir;
 
     let listener = UnixListener::bind("/tmp/salvo.sock").unwrap();
     let incoming = UnixListenerStream::new(listener);
-    let router = Router::new().path("files/<*path>").get(Static::from("./static"));
+    let router = Router::new().path("files/<*path>").get(StaticDir::new("./static"));
     Server::new(router).bind_incoming(incoming).await;
 }
 
