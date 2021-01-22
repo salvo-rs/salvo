bbec76402d55c4b81a784d8ec5dfd476f83b6ea9
chrislearn <chris@acroidea.com>
2021-01-22 16:02:34 +0800

    compression

---
+++ extra/src/compression/mod.rs
@@ -0,0 +1,139 @@
+//! Compress the body of a response.
+
+use async_compression::tokio::bufread::{BrotliEncoder, DeflateEncoder, GzipEncoder};
+use async_trait::async_trait;
+use http::header::HeaderValue;
+use hyper::header::{CONTENT_ENCODING, CONTENT_LENGTH, CONTENT_TYPE};
+use salvo_core::http::ResponseBody;
+use salvo_core::prelude::*;
+use tokio_stream::{self, StreamExt};
+use tokio_util::io::{ReaderStream, StreamReader};
+
+#[derive(Clone, Copy, Debug)]
+pub enum CompressionAlgo {
+    BR,
+    DEFLATE,
+    GZIP,
+}
+
+impl From<CompressionAlgo> for HeaderValue {
+    #[inline]
+    fn from(algo: CompressionAlgo) -> Self {
+        match algo {
+            CompressionAlgo::BR => HeaderValue::from_static("br"),
+            CompressionAlgo::DEFLATE => HeaderValue::from_static("deflate"),
+            CompressionAlgo::GZIP => HeaderValue::from_static("gzip"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CompressionHandler {
+    pub algo: CompressionAlgo,
+    pub content_types: Vec<String>,
+}
+
+impl CompressionHandler {
+    pub fn new(algo: CompressionAlgo) -> Self {
+        CompressionHandler {
+            algo,
+            content_types: vec![
+                "text/".into(),
+                "application/javascript".into(),
+                "application/json".into(),
+                "application/xml".into(),
+                "application/rss+xml".into(),
+                "image/svg+xml".into(),
+            ],
+        }
+    }
+    pub fn content_types(&self) -> &Vec<String> {
+        &self.content_types
+    }
+    pub fn content_types_mut(&mut self) -> &mut Vec<String> {
+        &mut self.content_types
+    }
+}
+
+#[async_trait]
+impl Handler for CompressionHandler {
+    async fn handle(&self, req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        let content_type = req.headers().get(CONTENT_TYPE).and_then(|v| v.to_str().ok()).unwrap_or_default();
+        if content_type.is_empty() || res.body().is_none() || !self.content_types.contains(&content_type.into()) {
+            return;
+        }
+        let body = res.take_body().unwrap();
+        if let ResponseBody::Empty = body {
+            return;
+        }
+        let body = body.map(|item|item.map_err(|_|std::io::ErrorKind::Other));
+        // let body: CompressableBody<ResponseBody, Box<dyn StdError + Send + Sync>> = CompressableBody { body };
+        match self.algo {
+            CompressionAlgo::GZIP => {
+                let stream = ReaderStream::new(GzipEncoder::new(StreamReader::new(body)));
+                res.streaming(stream);
+            }
+            CompressionAlgo::DEFLATE => {
+                let stream = ReaderStream::new(DeflateEncoder::new(StreamReader::new(body)));
+                res.streaming(stream);
+            }
+            CompressionAlgo::BR => {
+                let stream = ReaderStream::new(BrotliEncoder::new(StreamReader::new(body)));
+                res.streaming(stream);
+            }
+        }
+        res.headers_mut().remove(CONTENT_LENGTH);
+        res.headers_mut().append(CONTENT_ENCODING, self.algo.into());
+    }
+}
+
+/// Create a wrapping filter that compresses the Body of a [`Response`](crate::reply::Response)
+/// using gzip, adding `content-encoding: gzip` to the Response's [`HeaderMap`](hyper::HeaderMap)
+///
+/// # Example
+///
+/// ```
+/// use salvo_core::prelude::*;
+///
+/// let route = salvo::get()
+///     .and(salvo::path::end())
+///     .and(salvo::fs::file("./README.md"))
+///     .with(salvo::compression::gzip());
+/// ```
+pub fn gzip() -> CompressionHandler {
+    CompressionHandler::new(CompressionAlgo::GZIP)
+}
+
+/// Create a wrapping filter that compresses the Body of a [`Response`](crate::reply::Response)
+/// using deflate, adding `content-encoding: deflate` to the Response's [`HeaderMap`](hyper::HeaderMap)
+///
+/// # Example
+///
+/// ```
+/// use salvo_core::prelude::*;
+///
+/// let route = salvo::get()
+///     .and(salvo::path::end())
+///     .and(salvo::fs::file("./README.md"))
+///     .with(salvo::compression::deflate());
+/// ```
+pub fn deflate() -> CompressionHandler {
+    CompressionHandler::new(CompressionAlgo::DEFLATE)
+}
+
+/// Create a wrapping filter that compresses the Body of a [`Response`](crate::reply::Response)
+/// using brotli, adding `content-encoding: br` to the Response's [`HeaderMap`](hyper::HeaderMap)
+///
+/// # Example
+///
+/// ```
+/// use salvo_core::prelude::*;
+///
+/// let route = salvo::get()
+///     .and(salvo::path::end())
+///     .and(salvo::fs::file("./README.md"))
+///     .with(salvo::compression::brotli());
+/// ```
+pub fn brotli() -> CompressionHandler {
+    CompressionHandler::new(CompressionAlgo::BR)
+}
