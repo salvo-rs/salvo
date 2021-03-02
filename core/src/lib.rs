f2452d73b66e7c1fde5d6bc696d29bd8cea37a1d
Chrislearn Young <chris@acroidea.com>
2021-03-02 23:04:23 +0800

    split service from server

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,15 @@
 mod catcher;
-pub mod depot;
+mod depot;
 mod error;
 pub mod fs;
 mod handler;
 pub mod http;
 pub mod routing;
-pub mod server;
+mod server;
+mod service;
 #[cfg(feature = "tls")]
 mod tls;
-pub mod writer;
+mod writer;
 
 #[macro_use]
 extern crate pin_utils;
@@ -22,6 +23,7 @@ pub use self::handler::Handler;
 pub use self::http::{Request, Response};
 pub use self::routing::Router;
 pub use self::server::Server;
+pub use self::service::Service;
 #[cfg(feature = "tls")]
 pub use self::server::TlsServer;
 pub use self::writer::Writer;
@@ -34,6 +36,7 @@ pub mod prelude {
     pub use crate::routing::filter;
     pub use crate::routing::Router;
     pub use crate::server::Server;
+    pub use crate::service::Service;
     #[cfg(feature = "tls")]
     pub use crate::server::TlsServer;
     pub use crate::writer::*;
--- core/src/server.rs
+++ core/src/server.rs
@@ -3,57 +3,49 @@ use std::future::Future;
 use std::net::SocketAddr;
 #[cfg(feature = "tls")]
 use std::path::Path;
-use std::pin::Pin;
-use std::sync::Arc;
 
-use futures::{future, TryStream, TryStreamExt};
+use futures::{ TryStream, TryStreamExt};
 use hyper::server::accept::{self, Accept};
 use hyper::server::conn::AddrIncoming;
 use hyper::Server as HyperServer;
 use tokio::io::{AsyncRead, AsyncWrite};
 
-use crate::catcher;
-use crate::http::header::CONTENT_TYPE;
-use crate::http::{Mime, Request, Response, StatusCode};
-use crate::routing::{PathState, Router};
 #[cfg(feature = "tls")]
 use crate::tls::{TlsAcceptor, TlsConfigBuilder};
-use crate::{Catcher, Depot};
+use crate::Service;
 
 pub struct Server {
-    pub router: Arc<Router>,
-    pub catchers: Arc<Vec<Box<dyn Catcher>>>,
-    pub allowed_media_types: Arc<Vec<Mime>>,
+    service: Service,
 }
 
 impl Server {
-    pub fn new(router: Router) -> Server {
+    pub fn new(service: Service) -> Server {
         Server {
-            router: Arc::new(router),
-            catchers: Arc::new(catcher::defaults::get()),
-            allowed_media_types: Arc::new(vec![]),
+            service: service,
         }
+    } 
+    pub fn builder<I>(incoming: I) -> hyper::server::Builder<I> {
+        HyperServer::builder(incoming)
     }
-    fn create_bind_hyper_server(self, addr: impl Into<SocketAddr>) -> Result<(SocketAddr, hyper::Server<AddrIncoming, Self>), hyper::Error> {
+
+    fn create_bind_hyper_server(self, addr: impl Into<SocketAddr>) -> Result<(SocketAddr, hyper::Server<AddrIncoming, Service>), hyper::Error> {
         let addr = addr.into();
         let mut incoming = AddrIncoming::bind(&addr)?;
         incoming.set_nodelay(true);
-        let srv = HyperServer::builder(incoming).serve(self);
-        Ok((addr, srv))
+        Ok((addr, Self::builder(incoming).serve(self.service)))
     }
 
     #[inline]
     fn create_bind_incoming_hyper_server<S>(
         self,
         incoming: S,
-    ) -> Result<hyper::Server<impl Accept<Conn = S::Ok, Error = S::Error>, Self>, hyper::Error>
+    ) -> hyper::Server<impl Accept<Conn = S::Ok, Error = S::Error>, Service>
     where
         S: TryStream + Send,
         S::Ok: AsyncRead + AsyncWrite + Send + 'static + Unpin,
         S::Error: Into<Box<dyn StdError + Send + Sync>>,
     {
-        let srv = HyperServer::builder(accept::from_stream(incoming.into_stream())).serve(self);
-        Ok(srv)
+        Self::builder(accept::from_stream(incoming.into_stream())).serve(self.service)
     }
 
     /// Bind to a socket address, returning a `Future` that can be
@@ -106,7 +98,7 @@ impl Server {
     ///     let (tx, rx) = oneshot::channel();
     ///     let router = Router::new().get(hello_world);
     ///     let server = Server::new(router).bind_with_graceful_shutdown(([0, 0, 0, 0], 3131), async {
-    ///             rx.await.ok();
+    ///         rx.await.ok();
     ///     });
     ///
     ///     // Spawn the server into a runtime
@@ -163,7 +155,7 @@ impl Server {
         I::Ok: AsyncRead + AsyncWrite + Send + 'static + Unpin,
         I::Error: Into<Box<dyn StdError + Send + Sync>>,
     {
-        let srv = self.create_bind_incoming_hyper_server(incoming)?;
+        let srv = self.create_bind_incoming_hyper_server(incoming);
         tracing::info!("listening with custom incoming");
         if let Err(err) = srv.await {
             tracing::error!("server error: {}", err);
@@ -200,7 +192,7 @@ impl Server {
         I::Ok: AsyncRead + AsyncWrite + Send + 'static + Unpin,
         I::Error: Into<Box<dyn StdError + Send + Sync>>,
     {
-        let srv = self.create_bind_incoming_hyper_server(incoming)?;
+        let srv = self.create_bind_incoming_hyper_server(incoming);
         tracing::info!("listening with custom incoming");
         if let Err(err) = srv.with_graceful_shutdown(signal).await {
             tracing::error!("server error: {}", err);
@@ -216,37 +208,23 @@ impl Server {
     #[cfg(feature = "tls")]
     pub fn tls(self) -> TlsServer {
         TlsServer {
-            server: self,
+            service: self.service,
             builder: TlsConfigBuilder::new(),
         }
-    }
-}
-impl<T> hyper::service::Service<T> for Server {
-    type Response = HyperHandler;
-    type Error = std::io::Error;
-    // type Future = Pin<Box<(dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static)>>;
-    type Future = future::Ready<Result<Self::Response, Self::Error>>;
-
-    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
-        Ok(()).into()
-    }
-
-    fn call(&mut self, _: T) -> Self::Future {
-        future::ok(HyperHandler {
-            router: self.router.clone(),
-            catchers: self.catchers.clone(),
-            allowed_media_types: self.allowed_media_types.clone(),
-        })
     }
 }
 
 #[cfg(feature = "tls")]
 pub struct TlsServer {
-    server: Server,
+    service: Service,
     builder: TlsConfigBuilder,
 }
 #[cfg(feature = "tls")]
 impl TlsServer {
+    pub fn builder<I>(incoming: I) -> hyper::server::Builder<I> {
+        HyperServer::builder(incoming)
+    }
+
     // TLS config methods
 
     /// Specify the file path to read the private key.
@@ -328,19 +306,19 @@ impl TlsServer {
     where
         Func: FnOnce(TlsConfigBuilder) -> TlsConfigBuilder,
     {
-        let TlsServer { server, builder } = self;
+        let TlsServer { service, builder } = self;
         let builder = func(builder);
-        TlsServer { server, builder }
+        TlsServer { service, builder }
     }
 
     #[inline]
-    fn create_bind_hyper_server(self, addr: impl Into<SocketAddr>) -> Result<(SocketAddr, hyper::Server<TlsAcceptor, Server>), crate::Error> {
+    fn create_bind_hyper_server(self, addr: impl Into<SocketAddr>) -> Result<(SocketAddr, hyper::Server<TlsAcceptor, Service>), crate::Error> {
         let addr = addr.into();
-        let TlsServer { server, builder } = self;
+        let TlsServer { service, builder } = self;
         let tls = builder.build().map_err(crate::Error::new)?;
         let mut incoming = AddrIncoming::bind(&addr).map_err(crate::Error::new)?;
         incoming.set_nodelay(true);
-        let srv = HyperServer::builder(TlsAcceptor::new(tls, incoming)).serve(server);
+        let srv = Self::builder(TlsAcceptor::new(tls, incoming)).serve(service);
         Ok((addr, srv))
     }
     /// Bind to a socket address, returning a `Future` that can be
@@ -386,103 +364,5 @@ impl TlsServer {
         } else {
             Ok(addr)
         }
-    }
-}
-
-pub struct HyperHandler {
-    router: Arc<Router>,
-    catchers: Arc<Vec<Box<dyn Catcher>>>,
-    allowed_media_types: Arc<Vec<Mime>>,
-}
-#[allow(clippy::type_complexity)]
-impl hyper::service::Service<hyper::Request<hyper::body::Body>> for HyperHandler {
-    type Response = hyper::Response<hyper::body::Body>;
-    type Error = hyper::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
-
-    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
-        std::task::Poll::Ready(Ok(()))
-    }
-    fn call(&mut self, req: hyper::Request<hyper::body::Body>) -> Self::Future {
-        let catchers = self.catchers.clone();
-        let allowed_media_types = self.allowed_media_types.clone();
-        let mut request = Request::from_hyper(req).unwrap();
-        let mut response = Response::new(allowed_media_types.clone());
-        let mut depot = Depot::new();
-        let path = request.uri().path();
-        let segments = if let Some(path) = path.strip_prefix('/') { path.split('/') } else { path.split('/') };
-        let segments = segments
-            .map(|s| percent_encoding::percent_decode_str(s).decode_utf8_lossy().to_string())
-            .filter(|s| !s.contains('/') && !s.is_empty())
-            .collect::<Vec<_>>();
-        let mut path_state = PathState::new(segments);
-        response.cookies = request.cookies().clone();
-
-        let router = self.router.clone();
-        let fut = async move {
-            if let Some(dm) = router.detect(&mut request, &mut path_state) {
-                request.params = path_state.params;
-                for handler in [&dm.befores[..], &[dm.handler], &dm.afters[..]].concat() {
-                    handler.handle(&mut request, &mut depot, &mut response).await;
-                    if response.is_commited() {
-                        break;
-                    }
-                }
-                if !response.is_commited() {
-                    response.commit();
-                }
-            } else {
-                response.set_status_code(StatusCode::NOT_FOUND);
-            }
-
-            let mut hyper_response = hyper::Response::<hyper::Body>::new(hyper::Body::empty());
-
-            if response.status_code().is_none() {
-                if response.body.is_none() {
-                    response.set_status_code(StatusCode::NOT_FOUND);
-                } else {
-                    response.set_status_code(StatusCode::OK);
-                }
-            }
-            let status = response.status_code().unwrap();
-            let has_error = status.is_client_error() || status.is_server_error();
-            if let Some(value) = response.headers().get(CONTENT_TYPE) {
-                let mut is_allowed = false;
-                if let Ok(value) = value.to_str() {
-                    if allowed_media_types.is_empty() {
-                        is_allowed = true;
-                    } else {
-                        let ctype: Result<Mime, _> = value.parse();
-                        if let Ok(ctype) = ctype {
-                            for mime in &*allowed_media_types {
-                                if mime.type_() == ctype.type_() && mime.subtype() == ctype.subtype() {
-                                    is_allowed = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                if !is_allowed {
-                    response.set_status_code(StatusCode::UNSUPPORTED_MEDIA_TYPE);
-                }
-            } else {
-                tracing::warn!(
-                    uri = ?request.uri(),
-                    method = request.method().as_str(),
-                    "Http response content type header is not set"
-                );
-            }
-            if response.body.is_none() && has_error {
-                for catcher in &*catchers {
-                    if catcher.catch(&request, &mut response) {
-                        break;
-                    }
-                }
-            }
-            response.write_back(&mut request, &mut hyper_response).await;
-            Ok(hyper_response)
-        };
-        Box::pin(fut)
     }
 }
