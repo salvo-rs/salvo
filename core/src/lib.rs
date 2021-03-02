700e5c199d00b70e603af88a88fe7e709e1a02bd
Chrislearn Young <chris@acroidea.com>
2021-03-02 16:50:04 +0800

    remove unused string utils

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -27,58 +27,6 @@ pub use self::server::TlsServer;
 pub use self::writer::Writer;
 pub use salvo_macros::fn_handler;
 
-use std::ops::{Bound, RangeBounds};
-
-trait StringUtils {
-    fn substring(&self, start: usize, len: usize) -> &str;
-    fn slice(&self, range: impl RangeBounds<usize>) -> &str;
-}
-
-impl StringUtils for str {
-    fn substring(&self, start: usize, len: usize) -> &str {
-        let mut char_pos = 0;
-        let mut byte_start = 0;
-        let mut it = self.chars();
-        loop {
-            if char_pos == start {
-                break;
-            }
-            if let Some(c) = it.next() {
-                char_pos += 1;
-                byte_start += c.len_utf8();
-            } else {
-                break;
-            }
-        }
-        char_pos = 0;
-        let mut byte_end = byte_start;
-        loop {
-            if char_pos == len {
-                break;
-            }
-            if let Some(c) = it.next() {
-                char_pos += 1;
-                byte_end += c.len_utf8();
-            } else {
-                break;
-            }
-        }
-        &self[byte_start..byte_end]
-    }
-    fn slice(&self, range: impl RangeBounds<usize>) -> &str {
-        let start = match range.start_bound() {
-            Bound::Included(bound) | Bound::Excluded(bound) => *bound,
-            Bound::Unbounded => 0,
-        };
-        let len = match range.end_bound() {
-            Bound::Included(bound) => *bound + 1,
-            Bound::Excluded(bound) => *bound,
-            Bound::Unbounded => self.len(),
-        } - start;
-        self.substring(start, len)
-    }
-}
-
 pub mod prelude {
     pub use crate::depot::Depot;
     pub use crate::http::errors::*;
