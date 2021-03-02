7f3a206b54fe55e1125bf5f76df561cbec503c8c
Chrislearn Young <chris@acroidea.com>
2021-03-02 23:16:07 +0800

    make depot private

--- extra/src/auth/basic.rs
+++ extra/src/auth/basic.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 
-use salvo_core::depot::Depot;
+use salvo_core::Depot;
 use salvo_core::http::header::AUTHORIZATION;
 use salvo_core::http::{Request, Response, StatusCode};
 use salvo_core::Handler;
