f73249ca62426ad67456ab8fe427f591762afcc3
Chrislearn Young <chris@acroidea.com>
2021-03-04 09:48:33 +0800

    Update mod.rs
    Former-commit-id: aa668cd4063396dfad191ffe06c9f0d76f1af2cc

--- extra/src/proxy/mod.rs
+++ extra/src/proxy/mod.rs
@@ -1,15 +1,33 @@
 //! ProxyHandler.
 use async_trait::async_trait;
 use hyper::header::CONNECTION;
-use hyper::request::Parts;
+use hyper::Client;
 use salvo_core::prelude::*;
+use salvo_core::Result;
 use std::sync::{Arc, Mutex};
+use std::fmt;
 
 #[derive(Debug)]
+struct MsgError {
+    msg: String
+}
+
+impl MsgError {
+    fn new(msg: impl Into<String>) -> MsgError {
+        MsgError{msg: msg.into()}
+    }
+}
+
+impl fmt::Display for MsgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.msg)
+    }
+}
+
+impl std::error::Error for MsgError {}
+
 pub struct ProxyHandler {
     pub upstreams: Vec<String>,
-    pub request_modifier: Option<fn(&mut Request, &mut Depot)>,
-    pub response_modifier: Option<fn(&mut Response, &mut Depot)>,
     counter: Arc<Mutex<usize>>,
 }
 
@@ -20,20 +38,13 @@ impl ProxyHandler {
         }
         ProxyHandler {
             upstreams,
-            request_modifier: None,
-            response_modifier: None,
             counter: Arc::new(Mutex::new(0)),
         }
     }
 }
-
-#[async_trait]
-impl Handler for ProxyHandler {
-    async fn handle(&self, req: &mut Request, depot: &mut Depot, res: &mut Response) {
+impl ProxyHandler {
+    fn build_proxied_request(&self, req: &mut Request) -> Result<hyper::Request<hyper::body::Body>> {
         req.headers_mut().remove(CONNECTION);
-        if let Some(request_modifier) = self.request_modifier {
-            request_modifier(req, depot);
-        }
         let upstream = if self.upstreams.len() > 1 {
             let mut counter = self.counter.lock().unwrap();
             let upstream = self.upstreams.get(*counter);
@@ -43,65 +54,86 @@ impl Handler for ProxyHandler {
             self.upstreams.get(0)
         } else {
             tracing::error!("upstreams is empty");
-            return;
-        };
-        if let Some(upstream) = upstream {
-            let param = req.params().iter().find(|(key, _)| key.starts_with('*'));
-            let rest = if let Some((_, rest)) = param { rest } else { "".into() }.strip_prefix('/').unwrap_or("");
-            let forward_url = if let Some(query) = req.uri().query {
-                if rest.is_empty() {
-                    format!("{}?{}", upstream, query)
-                } else {
-                    format!("{}/{}?{}", upstream.strip_suffix('/').unwrap_or(""), rest, query)
-                }
+            return Err(salvo_core::Error::new(MsgError::new("upstreams is empty")));
+        }.map(|s|&**s).unwrap_or_default();
+        if upstream.is_empty() {
+            tracing::error!("upstreams is empty");
+            return Err(salvo_core::Error::new(MsgError::new("upstreams is empty")));
+        }
+
+        let param = req.params().iter().find(|(key, _)| key.starts_with('*'));
+        let rest = if let Some((_, rest)) = param { rest } else { "" }.strip_prefix('/').unwrap_or("");
+        let forward_url = if let Some(query) = req.uri().query() {
+            if rest.is_empty() {
+                format!("{}?{}", upstream, query)
             } else {
-                if rest.is_empty() {
-                    upstream.into()
-                } else {
-                    format!("{}{}", upstream.strip_suffix('/').unwrap_or(""), rest)
-                }
-            };
-            let (parts, body) = req.into_parts();
-            let mut proxied_request = Request::from_parts(parts, body);
-
-            // let x_forwarded_for_header_name = "x-forwarded-for";
-
-            // // Add forwarding information in the headers
-            // match request.headers_mut().entry(x_forwarded_for_header_name) {
-            //     Ok(header_entry) => {
-            //         match header_entry {
-            //             hyper::header::Entry::Vacant(entry) => {
-            //                 let addr = format!("{}", client_ip);
-            //                 entry.insert(addr.parse().unwrap());
-            //             },
-        
-            //             hyper::header::Entry::Occupied(mut entry) => {
-            //                 let addr = format!("{}, {}", entry.get().to_str().unwrap(), client_ip);
-            //                 entry.insert(addr.parse().unwrap());
-            //             }
-            //         }
-            //     }
-        
-            //     // shouldn't happen...
-            //     Err(_) => panic!("Invalid header name: {}", x_forwarded_for_header_name),
-            // }
-            let client = Client::new();
-	let proxied_response = client.request(proxied_request).then(|response| {
-		let proxied_response = match response {
-            Ok(response) => create_proxied_response(response),
-            Err(e) => {
-                tracing::error!("error: {}", e);
-                Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(Body::empty())
-                    .unwrap()
-            },
+                format!("{}/{}?{}", upstream.strip_suffix('/').unwrap_or(""), rest, query)
+            }
+        } else {
+            if rest.is_empty() {
+                upstream.into()
+            } else {
+                format!("{}{}", upstream.strip_suffix('/').unwrap_or(""), rest)
+            }
         };
+        let mut build = hyper::Request::builder().method(req.method()).version(req.version()).uri(forward_url);
+        for (key, value) in req.headers() {
+            build = build.header(key, value);
+        }
+        // let x_forwarded_for_header_name = "x-forwarded-for";
+        // // Add forwarding information in the headers
+        // match request.headers_mut().entry(x_forwarded_for_header_name) {
+        //     Ok(header_entry) => {
+        //         match header_entry {
+        //             hyper::header::Entry::Vacant(entry) => {
+        //                 let addr = format!("{}", client_ip);
+        //                 entry.insert(addr.parse().unwrap());
+        //             },
+        //             hyper::header::Entry::Occupied(mut entry) => {
+        //                 let addr = format!("{}, {}", entry.get().to_str().unwrap(), client_ip);
+        //                 entry.insert(addr.parse().unwrap());
+        //             }
+        //         }
+        //     }
+        //     // shouldn't happen...
+        //     Err(_) => panic!("Invalid header name: {}", x_forwarded_for_header_name),
+        // }
+        build.body(req.take_body().unwrap_or_default()).map_err(|e|salvo_core::Error::new(e))
+    }
+}
 
-        }.await?;
-        res.headers_mut().remove(CONNECTION);
-        if let Some(response_modifier) = self.response_modifier {
-            response_modifier(res, depot);
+#[async_trait]
+impl Handler for ProxyHandler {
+    async fn handle(&self, req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        match self.build_proxied_request(req) {
+            Ok(proxied_request) => {
+                let client = Client::new();
+                match client.request(proxied_request).await {
+                    Ok(response) => {
+                        let (
+                            http::response::Parts {
+                                status,
+                                // version,
+                                headers,
+                                // extensions,
+                                ..
+                            },
+                            body,
+                        ) = response.into_parts();
+                        res.set_status_code(status);
+                        res.set_headers(headers);
+                        res.set_body(Some(body.into()));
+                    }
+                    Err(e) => {
+                        tracing::error!("error: {}", e);
+                        res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
+                    }
+                };
+                res.headers_mut().remove(CONNECTION);
+            }
+            Err(e) => {
+                tracing::error!("error when build proxied request: {}", e);
+            }
         }
     }
 }
