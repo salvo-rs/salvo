f04cc16a0acebdd057d61a3cc76a0757bb5eed04
Chrislearn Young <chris@acroidea.com>
2021-03-04 10:29:52 +0800

    add proxy example
    Former-commit-id: 3d22aa74a6fc11d7f505859eab463f921939798a

---
+++ examples/proxy.rs
@@ -0,0 +1,21 @@
+use tracing_subscriber;
+use tracing_subscriber::fmt::format::FmtSpan;
+
+use salvo::prelude::*;
+use salvo_extra::proxy::ProxyHandler;
+
+#[fn_handler]
+async fn hello_world1(res: &mut Response) {
+    res.render_plain_text("Hello World1");
+}
+#[tokio::main]
+async fn main() {
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "proxy=debug,salvo=debug".to_owned());
+    tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
+    let router = Router::new().push(
+        Router::new()
+            .path("google/<*rest>")
+            .handle(ProxyHandler::new(vec!["https://www.google.com".into()])),
+    );
+    Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
+}
