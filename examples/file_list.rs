2a30b231aa9ecb2bbd0444a94da870332e2a9a64
Chrislearn Young <chris@acroidea.com>
2021-03-13 17:25:33 +0800

    add log
    Former-commit-id: c0fc0832f48fc1e65f8d22785fccc0e3fa340ccc

--- examples/file_list.rs
+++ examples/file_list.rs
@@ -1,9 +1,13 @@
 use salvo_core::routing::Router;
 use salvo_core::Server;
 use salvo_extra::serve::StaticDir;
+use tracing_subscriber;
+use tracing_subscriber::fmt::format::FmtSpan;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().path("files/<*path>").get(StaticDir::new(vec!["examples/static/body", "examples/static/girl"]));
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "hello_world=debug,salvo=debug".to_owned());
+    tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
+    let router = Router::new().path("<*path>").get(StaticDir::new(vec!["examples/static/body", "examples/static/girl"]));
     Server::new(router).bind(([0, 0, 0, 0], 9688)).await;
 }
