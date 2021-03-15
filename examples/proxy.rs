93e6c959a0ee7053a1c45329aff1fefd7e2e598f
Chrislearn Young <chris@acroidea.com>
2021-03-15 14:13:46 +0800

    Update proxy.rs

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -8,10 +8,16 @@ use salvo_extra::proxy::ProxyHandler;
 async fn main() {
     let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "proxy=debug,salvo=debug".to_owned());
     tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
-    let router = Router::new().push(
-        Router::new()
-            .path("google/<**rest>")
-            .handle(ProxyHandler::new(vec!["https://www.google.com".into()])),
-    );
+    let router = Router::new()
+        .push(
+            Router::new()
+                .path("google/<**rest>")
+                .handle(ProxyHandler::new(vec!["https://www.google.com".into()])),
+        )
+        .push(
+            Router::new()
+                .path("baidu/<**rest>")
+                .handle(ProxyHandler::new(vec!["https://www.baidu.com".into()])),
+        );
     Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
 }
