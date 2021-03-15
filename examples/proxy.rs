9b9fc9cdb79f643a72c1c0553ce8e98109831f43
Chrislearn Young <chris@acroidea.com>
2021-03-15 14:06:00 +0800

    Update proxy.rs
    Former-commit-id: 0dcfb1c7ea476ddf096c8eeaa9d435317fb41258

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -4,10 +4,6 @@ use tracing_subscriber::fmt::format::FmtSpan;
 use salvo::prelude::*;
 use salvo_extra::proxy::ProxyHandler;
 
-#[fn_handler]
-async fn hello_world1(res: &mut Response) {
-    res.render_plain_text("Hello World1");
-}
 #[tokio::main]
 async fn main() {
     let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "proxy=debug,salvo=debug".to_owned());
