d683707c4531a599add7210e7a661c982634e7f8
Chrislearn Young <chris@acroidea.com>
2021-03-19 05:58:28 +0800

    Update writer.rs
    Former-commit-id: 605b2d3f23589aff2443d8a608d020b0f9aad60d

--- core/src/writer.rs
+++ core/src/writer.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
+use serde::Serialize;
 
 use crate::http::header::HeaderValue;
+use crate::http::errors::*;
 use crate::http::{Request, Response};
 use crate::Depot;
 
@@ -10,31 +12,47 @@ pub trait Writer {
     async fn write(mut self, req: &mut Request, depot: &mut Depot, res: &mut Response);
 }
 
-pub struct HtmlTextContent<T>(T);
+#[allow(clippy::unit_arg)]
 #[async_trait]
-impl<T> Writer for HtmlTextContent<T>
-where
-    T: AsRef<str> + Send,
-{
-    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        res.render_binary(HeaderValue::from_static("text/html; charset=utf-8"), self.0.as_ref().as_bytes());
+impl Writer for () {
+    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, _res: &mut Response) {}
+}
+#[async_trait]
+impl<T, E> Writer for Result<T, E> where T: Writer + Send, E: Writer + Send {
+    async fn write(mut self, req: &mut Request, depot: &mut Depot, res: &mut Response) {
+        match self {
+            Ok(v) => {
+                v.write(req, depot, res).await;
+            }
+            Err(e) => {
+                e.write(req, depot, res).await;
+            }
+        }
     }
 }
 
-pub struct JsonTextContent<T>(T);
 #[async_trait]
-impl<T> Writer for JsonTextContent<T>
-where
-    T: AsRef<str> + Send,
-{
+impl<'a> Writer for &'a str {
     async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        res.render_binary(HeaderValue::from_static("application/json; charset=utf-8"), self.0.as_ref().as_bytes());
+        res.render_binary(HeaderValue::from_static("text/plain; charset=utf-8"), self.as_bytes());
+    }
+}
+#[async_trait]
+impl<'a> Writer for &'a String {
+    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        (&**self).write(_req, _depot, res).await;
+    }
+}
+#[async_trait]
+impl Writer for String {
+    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        (&*self).write(_req, _depot, res).await;
     }
 }
 
-pub struct PlainTextContent<T>(T);
+pub struct PlainText<T>(T);
 #[async_trait]
-impl<T> Writer for PlainTextContent<T>
+impl<T> Writer for PlainText<T>
 where
     T: AsRef<str> + Send,
 {
@@ -43,41 +61,32 @@ where
     }
 }
 
-pub struct XmlTextContent<T>(T);
+pub struct JsonText<T>(T);
 #[async_trait]
-impl<T> Writer for XmlTextContent<T>
+impl<T> Writer for JsonText<T>
+where
+    T: Serialize + Send,
+{
+    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        match serde_json::to_vec(&self.0) {
+            Ok(bytes) => {
+                res.render_binary(HeaderValue::from_static("application/json; charset=utf-8"), &bytes);
+            }
+            Err(e) => {
+                tracing::error!(error = ?e, "JsonText write error");
+                res.set_http_error(InternalServerError());
+            }
+        }
+    }
+}
+
+pub struct HtmlText<T>(T);
+#[async_trait]
+impl<T> Writer for HtmlText<T>
 where
     T: AsRef<str> + Send,
 {
     async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        res.render_binary(HeaderValue::from_static("text/xml; charset=utf-8"), self.0.as_ref().as_bytes());
-    }
-}
-
-#[allow(clippy::unit_arg)]
-#[async_trait]
-impl Writer for () {
-    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, _resp: &mut Response) {}
-}
-
-#[allow(clippy::unit_arg)]
-#[async_trait]
-impl<'a> Writer for &'a str {
-    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        res.render_binary(HeaderValue::from_static("text/plain; charset=utf-8"), self.as_bytes());
-    }
-}
-#[allow(clippy::unit_arg)]
-#[async_trait]
-impl<'a> Writer for &'a String {
-    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        (&**self).write(_req, _depot, res).await;
-    }
-}
-#[allow(clippy::unit_arg)]
-#[async_trait]
-impl Writer for String {
-    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-        (&*self).write(_req, _depot, res).await;
+        res.render_binary(HeaderValue::from_static("text/html; charset=utf-8"), self.0.as_ref().as_bytes());
     }
 }
