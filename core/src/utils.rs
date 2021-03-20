fb4a5ef80891ec11b0a89d04db3e248cd9d49035
Chrislearn Young <chris@acroidea.com>
2021-03-20 10:16:32 +0800

    decode_url_path_segments

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,8 +1,8 @@
 pub fn decode_url_path(path: &str) -> String {
-    format!("/{}", decode_url_segments(path).join("/"))
+    format!("/{}", decode_url_path_segments(path).join("/"))
 }
 
-pub fn decode_url_segments(path: &str) -> Vec<String> {
+pub fn decode_url_path_segments(path: &str) -> Vec<String> {
     let segments = path.trim_start_matches('/').split('/');
     segments
         .map(|s| percent_encoding::percent_decode_str(s).decode_utf8_lossy().to_string())
