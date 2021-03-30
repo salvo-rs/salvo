2587880711963ed37e64c72de686b3745dc83244
Chrislearn Young <chris@acroidea.com>
2021-03-30 21:53:20 +0800

    Update mod.rs

--- core/src/http/mod.rs
+++ core/src/http/mod.rs
@@ -1,16 +1,4 @@
-// #![feature(specialization)]
-// #![feature(proc_macro_hygiene)]
-// #![feature(crate_visibility_modifier)]
-// #![feature(doc_cfg)]
-// #![recursion_limit="512"]
-
-//! Types that map to concepts in HTTP.
-//!
-//! This module exports types that map to HTTP concepts or to the underlying
-//! HTTP library when needed. Because the underlying HTTP library is likely to
-//! change (see [#17]), types in [`hyper`] should be considered unstable.
-//!
-//! [#17]: https://github.com/SergioBenitez/Novel/issues/17
+//some code port from https://github.com/abonander/multipart-async
 
 pub mod body_chunk;
 pub mod errors;
