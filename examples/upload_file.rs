9b9a6ec59c6b4717a8275ad1c7e2b4c5358ec0ed
Chrislearn Young <chris@acroidea.com>
2021-03-31 21:33:05 +0800

    upload file examples

--- examples/upload_file.rs
+++ examples/upload_file.rs
@@ -14,11 +14,12 @@ async fn upload(req: &mut Request, res: &mut Response) {
     let file = req.get_file("file").await;
     if let Some(file) = file {
         let dest = format!("temp/{}", file.filename().unwrap_or_else(|| "file".into()));
+        println!("{}", dest);
         if let Err(e) = std::fs::copy(&file.path, Path::new(&dest)) {
             res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
             res.render_plain_text(&format!("file not found in request: {}", e.to_string()));
         } else {
-            res.render_plain_text("Ok");
+            res.render_plain_text(&format!("File uploaded to {}", dest));
         }
     } else {
         res.set_status_code(StatusCode::BAD_REQUEST);
@@ -28,8 +29,9 @@ async fn upload(req: &mut Request, res: &mut Response) {
 
 #[tokio::main]
 async fn main() {
-    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "hello_world=debug,salvo=debug".to_owned());
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "upload_file=debug,salvo=debug".to_owned());
     tracing_subscriber::fmt().with_env_filter(filter).with_span_events(FmtSpan::CLOSE).init();
+
     create_dir_all("temp").unwrap();
     let router = Router::new().get(index).post(upload);
     Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
@@ -38,13 +40,13 @@ async fn main() {
 static INDEX_HTML: &str = r#"<!DOCTYPE html>
 <html>
     <head>
-        <title>Upload</title>
+        <title>Upload file</title>
     </head>
     <body>
-        <h1>Upload</h1>
+        <h1>Upload file</h1>
         <form action="/" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" />
-        <input type="submit" />
+            <input type="file" name="file" />
+            <input type="submit" value="upload" />
         </form>
     </body>
 </html>
