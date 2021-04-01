baf73f945ed1726794808c3a9959aa310c4744a7
Chrislearn Young <chris@acroidea.com>
2021-04-01 09:40:46 +0800

    Update basic_auth.rs
    Former-commit-id: 053d651a2e5f0b27d02f3b4ee48d5f5254e2c72a

--- examples/basic_auth.rs
+++ examples/basic_auth.rs
@@ -8,7 +8,6 @@ async fn main()  {
     let baconfig = BasicAuthConfig {
         realm: "realm".to_owned(),
         context_key: Some("user_name".to_owned()),
-        expires: None,
         validator: Box::new(|user_name, password| -> bool { user_name == "root" && password == "pwd" }),
     };
     let auth_handler = BasicAuthHandler::new(baconfig);
