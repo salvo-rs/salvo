d260fb03382ca36afccd0e18dbaddec191639195
Chrislearn Young <chris@acroidea.com>
2021-04-03 15:39:02 +0800

    update for features

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 pub use salvo_core as core;
 pub use salvo_core::*;
 
-#[cfg(feature = "extra")]
+#[cfg(feature = "basic_auth")]
+#[cfg(feature = "jwt_auth")]
+#[cfg(feature = "compression")]
+#[cfg(feature = "proxy")]
+#[cfg(feature = "serve")]
+#[cfg(feature = "sse")]
+#[cfg(feature = "ws")]
+#[cfg(feature = "size_limiter")]
 pub use salvo_extra as extra;
