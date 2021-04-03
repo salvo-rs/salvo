f6814f473af825e5f9f57b1b68dee5a42db5c3ca
Chrislearn Young <chris@acroidea.com>
2021-04-03 11:48:27 +0800

    add size limiter and change folder structure
    Former-commit-id: 6f79afbf9de25b6e6071ef91d761ca1bf22622e6

---
+++ extra/src/size_limiter/mod.rs
@@ -0,0 +1,20 @@
+use async_trait::async_trait;
+use salvo_core::http::errors::*;
+use salvo_core::prelude::*;
+use salvo_core::http::HttpBody;
+
+pub struct MaxSizeHandler(u64);
+#[async_trait]
+impl Handler for MaxSizeHandler {
+    async fn handle(&self, req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        if let Some(upper) = req.body().and_then(|body|body.size_hint().upper()) {
+            if upper > self.0 {
+                res.set_http_error(PayloadTooLarge());
+            }
+        }
+    }
+}
+
+pub fn max_size(size: u64) -> MaxSizeHandler{
+    MaxSizeHandler(size)
+}
