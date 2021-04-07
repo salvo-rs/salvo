d50be8bc1d92bde8021269649141a156a1957ab2
Chrislearn Young <chris@acroidea.com>
2021-04-07 12:27:20 +0800

    License

--- core/src/tls.rs
+++ core/src/tls.rs
@@ -1,4 +1,7 @@
-//port from https://github.com/seanmonstar/warp/blob/master/src/tls.rs
+// Copyright (c) 2018-2020 Sean McArthur
+// Licensed under the MIT license http://opensource.org/licenses/MIT
+//
+// port from https://github.com/seanmonstar/warp/blob/master/src/tls.rs
 
 use std::fs::File;
 use std::future::Future;
