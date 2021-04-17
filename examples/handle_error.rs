625c534b3847a9ac7e1f15a7771930a4214e545c
Chrislearn Young <chris@acroidea.com>
2021-04-17 08:33:26 +0800

    Create handle_error.rs

---
+++ examples/handle_error.rs
@@ -0,0 +1,36 @@
+use salvo::prelude::*;
+use salvo::anyhow;
+use tracing_subscriber;
+use tracing_subscriber::fmt::format::FmtSpan;
+
+struct CustomError;
+#[async_trait]
+impl Writer for CustomError {
+    async fn write(mut self, _req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        res.render_plain_text("custom error");
+        res.set_http_error(InternalServerError());
+    }
+}
+
+#[fn_handler]
+async fn handle_anyhow() -> Result<(), anyhow::Error> {
+    Err(anyhow::anyhow!("anyhow error"))
+}
+#[fn_handler]
+async fn handle_custom() -> Result<(), CustomError> {
+    Err(CustomError)
+}
+
+#[tokio::main]
+async fn main() {
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "salvo=debug".to_owned());
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+    let router = Router::new()
+        .push(Router::new().path("anyhow").get(handle_anyhow))
+        .push(Router::new().path("custom").get(handle_custom));
+    Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
+}
