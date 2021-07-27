f2694ffc27315a760bdd139a10c702e8dd87db9f
Chrislearn Young <chris@acroidea.com>
2021-07-27 09:21:45 +0800

    set file list example list dir as default

--- examples/file_list.rs
+++ examples/file_list.rs
@@ -1,12 +1,17 @@
 use salvo_core::prelude::*;
-use salvo_extra::serve::StaticDir;
+use salvo_extra::serve::{Options, StaticDir};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new()
-        .path("<**path>")
-        .get(StaticDir::new(vec!["examples/static/body", "examples/static/girl"]));
+    let router = Router::new().path("<**path>").get(StaticDir::width_options(
+        vec!["examples/static/body", "examples/static/girl"],
+        Options {
+            dot_files: false,
+            listing: true,
+            defaults: vec!["index.html".to_owned()],
+        },
+    ));
     Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
 }
