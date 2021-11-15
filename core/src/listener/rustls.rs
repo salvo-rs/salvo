a4859343be536a5b42b4d91154754644c802a23e
Chrislearn Young <chris@acroidea.com>
2021-11-15 22:02:37 +0800

    pub use ServerConfig

--- core/src/listener/rustls.rs
+++ core/src/listener/rustls.rs
@@ -17,8 +17,9 @@ use rustls_pemfile::{self, pkcs8_private_keys, rsa_private_keys};
 use thiserror::Error;
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 use tokio_rustls::rustls::server::{
-    AllowAnyAnonymousOrAuthenticatedClient, AllowAnyAuthenticatedClient, NoClientAuth, ServerConfig,
+    AllowAnyAnonymousOrAuthenticatedClient, AllowAnyAuthenticatedClient, NoClientAuth,
 };
+pub use tokio_rustls::rustls::server::ServerConfig;
 use tokio_rustls::rustls::{Certificate, Error as RustlsError, PrivateKey, RootCertStore};
 
 use super::Listener;
