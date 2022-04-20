b3d69b1cc41754f4df62d77296b803075018257b
Chrislearn Young <chris@acroidea.com>
2022-04-21 06:54:07 +0800

    list style

--- extra/src/serve/dir.rs
+++ extra/src/serve/dir.rs
@@ -208,9 +208,7 @@ impl Handler for StaticDir {
                         if let Ok(named_file) = builder.build().await {
                             named_file.write(req, depot, res).await;
                         } else {
-                            res.set_status_error(
-                                StatusError::internal_server_error().with_summary("file read error"),
-                            );
+                            res.set_status_error(StatusError::internal_server_error().with_summary("file read error"));
                         }
                         return;
                     }
@@ -326,59 +324,26 @@ fn list_html(current: &CurrentInfo) -> String {
     fn header_links(path: &str) -> String {
         let segments = path.trim_start_matches('/').trim_end_matches('/').split('/');
         let mut link = "".to_string();
-        format!("{}{}", "<a href=\"/\"><svg aria-hidden=\"true\" data-icon=\"home\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\"><path fill=\"currentColor\" d=\"M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z\"></path></svg></a>", segments
-        .map(|seg| {
-            link = format!("{}/{}", link, seg);
-            format!("/<a href=\"{}\">{}</a>", link, seg)
-        }).collect::<Vec<_>>()
-        .join(""))
+        format!(
+            r#"<a href="/">{}</a>{}"#,
+            HOME_ICON,
+            segments
+                .map(|seg| {
+                    link = format!("{}/{}", link, seg);
+                    format!("/<a href=\"{}\">{}</a>", link, seg)
+                })
+                .collect::<Vec<_>>()
+                .join("")
+        )
     }
     let mut ftxt = format!(
-        r#"<!DOCTYPE html>
-<html>
-    <head>
+        r#"<!DOCTYPE html><html><head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
         <title>{}</title>
-        <style>
-        :root {{
-            --bg-color: #fff;
-            --text-color: #222;
-            --link-color: #0366d6;
-            --link-visited-color: #f22526;
-            --dir-icon-color: #79b8ff;
-            --file-icon-color: #959da5;
-        }}
-        body {{background: var(--bg-color); color: var(--text-color);}}
-        a{{text-decoration:none;color:var(--link-color);}}
-        a:visited {{color: var(--link-visited-color);}}
-        a:hover {{text-decoration:underline;}}
-        footer {{text-align:center;font-size:12px;}}
-        table {{text-align:left;border-collapse: collapse;}}
-        tr {{border-bottom: solid 1px #ccc;}}
-        tr:last-child {{border-bottom: none;}}
-        th, td {{padding: 5px;}}
-        th:first-child,td:first-child {{text-align: center;}}
-        svg[data-icon="dir"] {{vertical-align: text-bottom; color: var(--dir-icon-color); fill: currentColor;}}
-        svg[data-icon="file"] {{vertical-align: text-bottom; color: var(--file-icon-color); fill: currentColor;}}
-        svg[data-icon="home"] {{width:18px;}}
-        @media (prefers-color-scheme: dark) {{
-            :root {{
-                --bg-color: #222;
-                --text-color: #ddd;
-                --link-color: #539bf5;
-                --link-visited-color: #f25555;
-                --dir-icon-color: #7da3d0;
-                --file-icon-color: #545d68;
-            }}
-        }}
-        </style>
-    </head>
-    <body>
-        <header><h3>Index of: {}</h3></header>
-        <hr/>
-"#,
+        <style>{}</style></head><body><header><h3>Index of: {}</h3></header><hr/>"#,
         current.path,
+        HTML_STYLE,
         header_links(&current.path)
     );
     if current.dirs.is_empty() && current.files.is_empty() {
@@ -418,5 +383,39 @@ fn list_text(current: &CurrentInfo) -> String {
     json!(current).to_string()
 }
 
+const HTML_STYLE: &str = r#"
+    :root {
+        --bg-color: #fff;
+        --text-color: #222;
+        --link-color: #0366d6;
+        --link-visited-color: #f22526;
+        --dir-icon-color: #79b8ff;
+        --file-icon-color: #959da5;
+    }
+    body {background: var(--bg-color); color: var(--text-color);}
+    a {text-decoration:none;color:var(--link-color);}
+    a:visited {color: var(--link-visited-color);}
+    a:hover {text-decoration:underline;}
+    header a {padding: 0 6px;}
+    footer {text-align:center;font-size:12px;}
+    table {text-align:left;border-collapse: collapse;}
+    tr {border-bottom: solid 1px #ccc;}
+    tr:last-child {border-bottom: none;}
+    th, td {padding: 5px;}
+    th:first-child,td:first-child {text-align: center;}
+    svg[data-icon="dir"] {vertical-align: text-bottom; color: var(--dir-icon-color); fill: currentColor;}
+    svg[data-icon="file"] {vertical-align: text-bottom; color: var(--file-icon-color); fill: currentColor;}
+    svg[data-icon="home"] {width:18px;}
+    @media (prefers-color-scheme: dark) {
+        :root {
+            --bg-color: #222;
+            --text-color: #ddd;
+            --link-color: #539bf5;
+            --link-visited-color: #f25555;
+            --dir-icon-color: #7da3d0;
+            --file-icon-color: #545d68;
+        }}
+    }"#;
 const DIR_ICON: &str = r#"<svg aria-label="Directory" data-icon="dir" width="20" height="20" viewBox="0 0 512 512" version="1.1" role="img"><path fill="currentColor" d="M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"></path></svg>"#;
 const FILE_ICON: &str = r#"<svg aria-label="File" data-icon="file" width="20" height="20" viewBox="0 0 384 512" version="1.1" role="img"><path d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48z"/></svg>"#;
+const HOME_ICON: &str = r#"<svg aria-hidden="true" data-icon="home" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>"#;
