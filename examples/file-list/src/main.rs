84a35be0d4614135f759fa43e784f95015a5fc80
Fergus Hou <fergus.hou@outlook.com>
2022-05-15 17:30:18 +0800

    Fix error path

--- examples/file-list/src/main.rs
+++ examples/file-list/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     let router = Router::with_path("<**path>").get(DirHandler::width_options(
-        vec!["examples/static/boy", "examples/static/girl"],
+        vec!["examples/file-list/static/boy", "examples/file-list/static/girl"],
         Options {
             dot_files: false,
             listing: true,
