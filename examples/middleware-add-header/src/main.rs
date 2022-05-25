2545bf7df2288b151a8f491f139a7cfbc1075135
Chrislearn Young <chris@acroidea.com>
2022-05-25 10:50:19 +0800

    remove route()

--- examples/middleware-add-header/src/main.rs
+++ examples/middleware-add-header/src/main.rs
@@ -17,9 +17,6 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     tracing::info!("Listening on http://127.0.0.1:7878");
-    Server::new(TcpListener::bind("127.0.0.1:7878")).serve(route()).await;
-}
-
-fn route() -> Router {
-    Router::new().hoop(add_header).get(hello_world)
+    let router = Router::new().hoop(add_header).get(hello_world);
+    Server::new(TcpListener::bind("127.0.0.1:7878")).serve(router).await;
 }
