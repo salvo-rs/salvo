0e8e9584ff8b75b147c4372bf354f678861fa5dc
Chrislearn Young <chris@kenorld.com>
2022-06-28 05:04:39 +0800

    WIP

--- core/src/serde/de.rs
+++ core/src/serde/de.rs
@@ -36,7 +36,14 @@ where
         let value = value.expect("MapAccess::next_value called before next_key");
         let source = self.field_source.take().or(self.default_source()).unwrap();
         let field = &de.metadata.fields[self.field_index];
-        match f
+        match &*source.name {
+            "json" => {
+                Ok(value.into_deserializer())
+            }
+            _ => {
+                Ok(CowValue(Cow::from(Value)))
+            }
+        }
     }
     fn  next_pair(&mut self) -> Option<(&'a str, &'a str)> {
         if self.field_index < de.metadata.fields.len() - 1 {
@@ -44,29 +51,37 @@ where
                 self.field_index += 1;
                 let field = &de.metadata.fields[self.field_index];
                 for source in &field.sources {
-                    let value = match &*source.name {
-                        "params" => {
+                    let value = match &*source.from {
+                        "param" => {
                             if let Some(value) = req.params().get(field.name) {
-                                self.field_source = Some(source);
                                 return  Some((field.name, value));
                             }
                         }
-                        "queries" => {
+                        "query" => {
                             if let Some(value) = value = req.queries().get(field.name) {
-                                all_data.insert(field.name, value);
+                                return  Some((field.name, value));
                             }
                         }
-                        "headers" => {
-                            let value = req.http_header(field.name).await?;
-                            all_data.insert(field.name, value);
+                        "header" => {
+                            if let Some(value) =  req.header(field.name) {
+                                return  Some((field.name, value));
+                            }
                         }
-                        "body_form" => {
-                            let value = req.http_header(field.name).await?;
-                            all_data.insert(field.name, value);
-                        }
-                        "body_json" => {
-                            let value = req.form_data(field.name).await?;
-                            all_data.insert(field.name, value);
+                        "body" => {
+                            match source.format {
+                                Some("json") => {
+                                    if let Some(json_body) =  &self.json_body {
+                                        return  Some((field.name, ""));
+                                    }
+                                }
+                                Some("form") => {
+                                    let value = req.json().await?;
+                                    all_data.insert(field.name, value);
+                                }
+                                _ => {
+                                    tracing::error!("Unsupported source format: {}", source.format);
+                                }
+                            }
                         }
     
                         if let Some(value) = source.get(req, &field.name) {
