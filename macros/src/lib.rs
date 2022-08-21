234637e0542e7250fa51a42c970d0db91a8b9e4b
Chrislearn Young <chris@acroidea.com>
2022-08-21 10:07:39 +0800

    unit test: test_handler_for_fn_return_result

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -114,6 +114,52 @@ mod tests {
     }
 
     #[test]
+    fn test_handler_for_fn_return_result() {
+        let input = quote! {
+            #[handler]
+            async fn hello(req: &mut Request, depot: &mut Depot, res: &mut Response, ctrl: &mut FlowCtrl) -> Result<(), Error> {
+                Ok(())
+            }
+        };
+        let item = parse2(input).unwrap();
+        assert_eq!(
+            handler::generate(false, item).unwrap().to_string(),
+            quote! {
+                #[allow(non_camel_case_types)]
+                #[derive(Debug)]
+                struct hello;
+                impl hello {
+                    #[handler]
+                    async fn hello(
+                        req: &mut Request,
+                        depot: &mut Depot,
+                        res: &mut Response,
+                        ctrl: &mut FlowCtrl
+                    ) -> Result<(), Error> {
+                        {
+                            Ok(())
+                        }
+                    }
+                }
+                #[salvo::async_trait]
+                impl salvo::Handler for hello {
+                    #[inline]
+                    async fn handle(
+                        &self,
+                        req: &mut salvo::Request,
+                        depot: &mut salvo::Depot,
+                        res: &mut salvo::Response,
+                        ctrl: &mut salvo::routing::FlowCtrl
+                    ) {
+                        salvo::Writer::write(Self::hello(req, depot, res, ctrl).await, req, depot, res).await;
+                    }
+                }
+            }
+            .to_string()
+        );
+    }
+
+    #[test]
     fn test_handler_for_impl() {
         let input = quote! {
             #[handler]
