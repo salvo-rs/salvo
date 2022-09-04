cb0ebfb6a94e8c03a716fffadbf96c5e15285763
Chrislearn Young <chris@acroidea.com>
2022-09-04 17:52:41 +0800

    Add fallback keys

--- extra/src/session.rs
+++ extra/src/session.rs
@@ -74,7 +74,7 @@ use async_session::sha2::Sha256;
 use cookie::{Cookie, Key, SameSite};
 use salvo_core::http::uri::Scheme;
 use salvo_core::routing::FlowCtrl;
-use salvo_core::{async_trait, Depot, Handler, Request, Response};
+use salvo_core::{async_trait, Depot, Error, Handler, Request, Response};
 
 /// Key for store data in depot.
 pub const SESSION_KEY: &str = "::salvo::extra::session";
@@ -111,6 +111,166 @@ impl SessionDepotExt for Depot {
     }
 }
 
+/// HandlerBuilder
+pub struct HandlerBuilder<S> {
+    store: S,
+    cookie_path: String,
+    cookie_name: String,
+    cookie_domain: Option<String>,
+    session_ttl: Option<Duration>,
+    save_unchanged: bool,
+    same_site_policy: SameSite,
+    key: Key,
+    fallback_keys: Vec<Key>,
+}
+impl<S: SessionStore> fmt::Debug for HandlerBuilder<S> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("HandlerBuilder")
+            .field("store", &self.store)
+            .field("cookie_path", &self.cookie_path)
+            .field("cookie_name", &self.cookie_name)
+            .field("cookie_domain", &self.cookie_domain)
+            .field("session_ttl", &self.session_ttl)
+            .field("same_site_policy", &self.same_site_policy)
+            .field("key", &"..")
+            .field("fallback_keys", &"..")
+            .field("save_unchanged", &self.save_unchanged)
+            .finish()
+    }
+}
+
+impl<S> HandlerBuilder<S>
+where
+    S: SessionStore,
+{
+    /// Create new `HandlerBuilder`
+    #[inline]
+    pub fn new(store: S, secret: &[u8]) -> Self {
+        Self {
+            store,
+            save_unchanged: true,
+            cookie_path: "/".into(),
+            cookie_name: "salvo.sid".into(),
+            cookie_domain: None,
+            same_site_policy: SameSite::Lax,
+            session_ttl: Some(Duration::from_secs(24 * 60 * 60)),
+            key: Key::from(secret),
+            fallback_keys: vec![],
+        }
+    }
+
+    /// Set a cookie path for this session middleware.
+    ///
+    /// The default for this value is "/".
+    #[inline]
+    pub fn cookie_path(mut self, cookie_path: impl AsRef<str>) -> Self {
+        self.cookie_path = cookie_path.as_ref().to_owned();
+        self
+    }
+
+    /// Set a session ttl. This will be used both for the cookie
+    /// expiry and also for the session-internal expiry.
+    ///
+    /// The default for this value is one day. Set this to None to not
+    /// set a cookie or session expiry. This is not recommended.
+    #[inline]
+    pub fn session_ttl(mut self, session_ttl: Option<Duration>) -> Self {
+        self.session_ttl = session_ttl;
+        self
+    }
+
+    /// Set the name of the cookie that the session is stored with or in.
+    ///
+    /// If you are running multiple tide applications on the same
+    /// domain, you will need different values for each
+    /// application. The default value is "tide.sid".
+    #[inline]
+    pub fn cookie_name(mut self, cookie_name: impl AsRef<str>) -> Self {
+        self.cookie_name = cookie_name.as_ref().to_owned();
+        self
+    }
+
+    /// Set the `save_unchanged` value.
+    ///
+    /// When `save_unchanged` is enabled, a session will cookie will always be set.
+    ///
+    /// With `save_unchanged` disabled, the session data must be modified
+    /// from the `Default` value in order for it to save. If a session
+    /// already exists and its data unmodified in the course of a
+    /// request, the session will only be persisted if
+    /// `save_unchanged` is enabled.
+    #[inline]
+    pub fn save_unchanged(mut self, value: bool) -> Self {
+        self.save_unchanged = value;
+        self
+    }
+
+    /// Set the same site policy for the session cookie. Defaults to
+    /// SameSite::Lax. See [incrementally better
+    /// cookies](https://tools.ietf.org/html/draft-west-cookie-incrementalism-01)
+    /// for more information about this setting.
+    #[inline]
+    pub fn same_site_policy(mut self, policy: SameSite) -> Self {
+        self.same_site_policy = policy;
+        self
+    }
+
+    /// Set the domain of the cookie.
+    #[inline]
+    pub fn cookie_domain(mut self, cookie_domain: impl AsRef<str>) -> Self {
+        self.cookie_domain = Some(cookie_domain.as_ref().to_owned());
+        self
+    }
+    /// Set fallbacks.
+    #[inline]
+    pub fn fallback_keys(mut self, keys: Vec<impl Into<Key>>) -> Self {
+        self.fallback_keys = keys.into_iter().map(|s| s.into()).collect();
+        self
+    }
+
+    /// Add fallback secret.
+    #[inline]
+    pub fn add_fallback_key(mut self, key: impl Into<Key>) -> Self {
+        self.fallback_keys.push(key.into());
+        self
+    }
+
+    /// Build `SessionHandler`
+    #[inline]
+    pub fn build(self) -> Result<SessionHandler<S>, Error> {
+        let Self {
+            store,
+            save_unchanged,
+            cookie_path,
+            cookie_name,
+            cookie_domain,
+            session_ttl,
+            same_site_policy,
+            key,
+            fallback_keys,
+        } = self;
+        let hmac =
+            Hmac::<Sha256>::new_from_slice(key.signing()).map_err(|_| Error::Other("invalid key length".into()))?;
+        let fallback_hmacs = fallback_keys
+            .iter()
+            .map(|key| Hmac::<Sha256>::new_from_slice(key.signing()))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|_| Error::Other("invalid key length".into()))?;
+        Ok(SessionHandler {
+            store,
+            save_unchanged,
+            cookie_path,
+            cookie_name,
+            cookie_domain,
+            session_ttl,
+            same_site_policy,
+            hmac,
+            fallback_hmacs,
+        })
+    }
+}
+
 /// SessionHandler
 pub struct SessionHandler<S> {
     store: S,
@@ -120,12 +280,13 @@ pub struct SessionHandler<S> {
     session_ttl: Option<Duration>,
     save_unchanged: bool,
     same_site_policy: SameSite,
-    key: Key,
+    hmac: Hmac<Sha256>,
+    fallback_hmacs: Vec<Hmac<Sha256>>,
 }
 impl<S: SessionStore> fmt::Debug for SessionHandler<S> {
     #[inline]
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("SessionManger")
+        f.debug_struct("SessionHandler")
             .field("store", &self.store)
             .field("cookie_path", &self.cookie_path)
             .field("cookie_name", &self.cookie_name)
@@ -133,6 +294,7 @@ impl<S: SessionStore> fmt::Debug for SessionHandler<S> {
             .field("session_ttl", &self.session_ttl)
             .field("same_site_policy", &self.same_site_policy)
             .field("key", &"..")
+            .field("fallback_keys", &"..")
             .field("save_unchanged", &self.save_unchanged)
             .finish()
     }
@@ -186,81 +348,9 @@ impl<S> SessionHandler<S>
 where
     S: SessionStore,
 {
-    /// Create new `SessionHandler`
-    #[inline]
-    pub fn new(store: S, secret: &[u8]) -> Self {
-        Self {
-            store,
-            save_unchanged: true,
-            cookie_path: "/".into(),
-            cookie_name: "salvo.sid".into(),
-            cookie_domain: None,
-            same_site_policy: SameSite::Lax,
-            session_ttl: Some(Duration::from_secs(24 * 60 * 60)),
-            key: Key::from(secret),
-        }
-    }
-    /// Sets a cookie path for this session middleware.
-    ///
-    /// The default for this value is "/".
-    #[inline]
-    pub fn with_cookie_path(mut self, cookie_path: impl AsRef<str>) -> Self {
-        self.cookie_path = cookie_path.as_ref().to_owned();
-        self
-    }
-
-    /// Sets a session ttl. This will be used both for the cookie
-    /// expiry and also for the session-internal expiry.
-    ///
-    /// The default for this value is one day. Set this to None to not
-    /// set a cookie or session expiry. This is not recommended.
-    #[inline]
-    pub fn with_session_ttl(mut self, session_ttl: Option<Duration>) -> Self {
-        self.session_ttl = session_ttl;
-        self
-    }
-
-    /// Sets the name of the cookie that the session is stored with or in.
-    ///
-    /// If you are running multiple tide applications on the same
-    /// domain, you will need different values for each
-    /// application. The default value is "tide.sid".
-    #[inline]
-    pub fn with_cookie_name(mut self, cookie_name: impl AsRef<str>) -> Self {
-        self.cookie_name = cookie_name.as_ref().to_owned();
-        self
-    }
-
-    /// Disables the `save_unchanged` setting.
-    ///
-    /// When `save_unchanged` is enabled, a session will cookie will always be set.
-    /// 
-    /// With `save_unchanged` disabled, the session data must be modified
-    /// from the `Default` value in order for it to save. If a session
-    /// already exists and its data unmodified in the course of a
-    /// request, the session will only be persisted if
-    /// `save_unchanged` is enabled.
-    #[inline]
-    pub fn without_save_unchanged(mut self) -> Self {
-        self.save_unchanged = false;
-        self
-    }
-
-    /// Sets the same site policy for the session cookie. Defaults to
-    /// SameSite::Lax. See [incrementally better
-    /// cookies](https://tools.ietf.org/html/draft-west-cookie-incrementalism-01)
-    /// for more information about this setting.
-    #[inline]
-    pub fn with_same_site_policy(mut self, policy: SameSite) -> Self {
-        self.same_site_policy = policy;
-        self
-    }
-
-    /// Sets the domain of the cookie.
-    #[inline]
-    pub fn with_cookie_domain(mut self, cookie_domain: impl AsRef<str>) -> Self {
-        self.cookie_domain = Some(cookie_domain.as_ref().to_owned());
-        self
+    /// Create new `HandlerBuilder`
+    pub fn builder(store: S, secret: &[u8]) -> HandlerBuilder<S> {
+        HandlerBuilder::new(store, secret)
     }
     #[inline]
     async fn load_or_create(&self, cookie_value: Option<String>) -> Session {
@@ -277,21 +367,29 @@ where
     /// verifies the signed value and returns it. If there's a problem, returns
     /// an `Err` with a string describing the issue.
     #[inline]
-    fn verify_signature(&self, cookie_value: &str) -> Result<String, &'static str> {
+    fn verify_signature(&self, cookie_value: &str) -> Result<String, Error> {
         if cookie_value.len() < BASE64_DIGEST_LEN {
-            return Err("length of value is <= BASE64_DIGEST_LEN");
+            return Err(Error::Other("length of value is <= BASE64_DIGEST_LEN".into()));
         }
 
         // Split [MAC | original-value] into its two parts.
         let (digest_str, value) = cookie_value.split_at(BASE64_DIGEST_LEN);
-        let digest = base64::decode(digest_str).map_err(|_| "bad base64 digest")?;
+        let digest = base64::decode(digest_str).map_err(|_| Error::Other("bad base64 digest".into()))?;
 
         // Perform the verification.
-        let mut mac = Hmac::<Sha256>::new_from_slice(self.key.signing()).expect("good key");
-        mac.update(value.as_bytes());
-        mac.verify(&digest)
-            .map(|_| value.to_string())
-            .map_err(|_| "value did not verify")
+        let mut hmac = self.hmac.clone();
+        hmac.update(value.as_bytes());
+        if hmac.verify(&digest).is_ok() {
+            return Ok(value.to_string());
+        }
+        for hmac in &self.fallback_hmacs {
+            let mut hmac = hmac.clone();
+            hmac.update(value.as_bytes());
+            if hmac.verify(&digest).is_ok() {
+                return Ok(value.to_string());
+            }
+        }
+        Err(Error::Other("value did not verify".into()))
     }
     #[inline]
     fn build_cookie(&self, secure: bool, cookie_value: String) -> Cookie<'static> {
@@ -320,7 +418,7 @@ where
     #[inline]
     fn sign_cookie(&self, cookie: &mut Cookie<'_>) {
         // Compute HMAC-SHA256 of the cookie's value.
-        let mut mac = Hmac::<Sha256>::new_from_slice(self.key.signing()).expect("good key");
+        let mut mac = self.hmac.clone();
         mac.update(cookie.value().as_bytes());
 
         // Cookie's new value is [MAC | original-value].
@@ -330,21 +428,26 @@ where
     }
 }
 
-#[test]
-fn test_session_data() {
-    let handler = SessionHandler::new(
-        async_session::CookieStore,
-        b"secretabsecretabsecretabsecretabsecretabsecretabsecretabsecretab",
-    )
-    .with_cookie_domain("test.domain")
-    .with_cookie_name("test_cookie")
-    .with_cookie_path("/abc")
-    .with_same_site_policy(SameSite::Strict)
-    .with_session_ttl(Some(Duration::from_secs(30)));
-    assert_eq!(handler.cookie_domain, Some("test.domain".into()));
-    assert_eq!(handler.cookie_name, "test_cookie");
-    assert_eq!(handler.cookie_path, "/abc");
-    assert_eq!(handler.same_site_policy, SameSite::Strict);
-    assert_eq!(handler.session_ttl, Some(Duration::from_secs(30)));
-    assert!(handler.key == Key::from(b"secretabsecretabsecretabsecretabsecretabsecretabsecretabsecretab"));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_session_data() {
+        let handler = SessionHandler::builder(
+            async_session::CookieStore,
+            b"secretabsecretabsecretabsecretabsecretabsecretabsecretabsecretab",
+        )
+        .cookie_domain("test.domain")
+        .cookie_name("test_cookie")
+        .cookie_path("/abc")
+        .same_site_policy(SameSite::Strict)
+        .session_ttl(Some(Duration::from_secs(30)))
+        .build().unwrap();
+        assert_eq!(handler.cookie_domain, Some("test.domain".into()));
+        assert_eq!(handler.cookie_name, "test_cookie");
+        assert_eq!(handler.cookie_path, "/abc");
+        assert_eq!(handler.same_site_policy, SameSite::Strict);
+        assert_eq!(handler.session_ttl, Some(Duration::from_secs(30)));
+    }
 }
