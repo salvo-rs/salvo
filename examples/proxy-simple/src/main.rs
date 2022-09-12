c20daf59c1473af23dcf7afeb17c6ffc5d62ffcc
Chrislearn Young <chris@acroidea.com>
2022-09-12 17:07:02 +0800

    Update main.rs

--- examples/proxy-simple/src/main.rs
+++ examples/proxy-simple/src/main.rs
@@ -6,8 +6,8 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     let router = Router::new()
-        .push(Router::with_path("google/<**rest>").handle(Proxy::new(vec!["https://www.google.com".into()])))
-        .push(Router::with_path("baidu/<**rest>").handle(Proxy::new(vec!["https://www.baidu.com".into()])));
+        .push(Router::with_path("google/<**rest>").handle(Proxy::new(["https://www.google.com"])))
+        .push(Router::with_path("baidu/<**rest>").handle(Proxy::new("https://www.baidu.com")));
     println!("{:?}", router);
     tracing::info!("Listening on http://127.0.0.1:7878");
     Server::new(TcpListener::bind("127.0.0.1:7878")).serve(router).await;
