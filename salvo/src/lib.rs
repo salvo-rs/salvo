9bdd0f4db1fb182764c710d2d745b3f5b4885835
Chrislearn Young <chris@acroidea.com>
2022-09-13 04:00:46 +0800

    Version 0.34.2

--- salvo/src/lib.rs
+++ salvo/src/lib.rs
@@ -84,7 +84,7 @@ pub mod prelude {
     }
     cfg_feature! {
         #![feature ="serve-static"]
-        pub use salvo_extra::serve_static::{StaticDir, StaticFile, static_embed, static_embed_with_index, StaticEmbed};
+        pub use salvo_extra::serve_static::{StaticDir, StaticFile, static_embed, StaticEmbed};
     }
     cfg_feature! {
         #![feature ="session"]
