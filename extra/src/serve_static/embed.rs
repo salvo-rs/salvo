9fee56c01ece6cc4d4cdc7271761f4a492c58896
Chrislearn Young <chris@acroidea.com>
2022-09-14 15:18:01 +0800

    Unit tests

--- extra/src/serve_static/embed.rs
+++ extra/src/serve_static/embed.rs
@@ -21,6 +21,35 @@ pub fn static_embed<T: RustEmbed>() -> StaticEmbed<T> {
     }
 }
 
+/// Render [`EmbeddedFile`] to [`Response`].
+pub fn render_embedded_file(file: EmbeddedFile, req: &Request, res: &mut Response, mime: Option<Mime>) {
+    let hash = hex::encode(file.metadata.sha256_hash());
+    // if etag is matched, return 304
+    if req
+        .headers()
+        .get(IF_NONE_MATCH)
+        .map(|etag| etag.to_str().unwrap_or("000000").eq(&hash))
+        .unwrap_or(false)
+    {
+        res.set_status_code(StatusCode::NOT_MODIFIED);
+        return;
+    }
+
+    // otherwise, return 200 with etag hash
+    res.headers_mut().insert(ETAG, hash.parse().unwrap());
+
+    let mime = mime.unwrap_or_else(|| mime_guess::from_path(req.uri().path()).first_or_octet_stream());
+    res.headers_mut().insert(CONTENT_TYPE, mime.as_ref().parse().unwrap());
+    match file.data {
+        Cow::Borrowed(data) => {
+            res.write_body(data).ok();
+        }
+        Cow::Owned(data) => {
+            res.write_body(data).ok();
+        }
+    }
+}
+
 impl<T> StaticEmbed<T>
 where
     T: RustEmbed + Send + Sync + 'static,
@@ -91,34 +120,5 @@ pub trait EmbeddedFileExt {
 impl EmbeddedFileExt for EmbeddedFile {
     fn render(self, req: &Request, res: &mut Response) {
         render_embedded_file(self, req, res, None);
-    }
-}
-
-/// Render [`EmbeddedFile`] to [`Response`].
-pub fn render_embedded_file(file: EmbeddedFile, req: &Request, res: &mut Response, mime: Option<Mime>) {
-    let hash = hex::encode(file.metadata.sha256_hash());
-    // if etag is matched, return 304
-    if req
-        .headers()
-        .get(IF_NONE_MATCH)
-        .map(|etag| etag.to_str().unwrap_or("000000").eq(&hash))
-        .unwrap_or(false)
-    {
-        res.set_status_code(StatusCode::NOT_MODIFIED);
-        return;
-    }
-
-    // otherwise, return 200 with etag hash
-    res.headers_mut().insert(ETAG, hash.parse().unwrap());
-
-    let mime = mime.unwrap_or_else(|| mime_guess::from_path(req.uri().path()).first_or_octet_stream());
-    res.headers_mut().insert(CONTENT_TYPE, mime.as_ref().parse().unwrap());
-    match file.data {
-        Cow::Borrowed(data) => {
-            res.write_body(data).ok();
-        }
-        Cow::Owned(data) => {
-            res.write_body(data).ok();
-        }
     }
 }
--- extra/src/serve_static/mod.rs
+++ extra/src/serve_static/mod.rs
@@ -42,14 +42,16 @@ pub(crate) fn format_url_path_safely(path: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::serve_static::*;
+    use rust_embed::RustEmbed;
     use salvo_core::prelude::*;
     use salvo_core::test::{ResponseExt, TestClient};
+
+    use crate::serve_static::*;
 
     #[tokio::test]
     async fn test_serve_static_files() {
         let router = Router::with_path("<**path>").get(StaticDir::width_options(
-            vec!["../examples/static-dir-list/static/test"],
+            vec!["test/static"],
             StaticDirOptions {
                 dot_files: false,
                 listing: true,
@@ -68,16 +70,19 @@ mod tests {
                 .unwrap()
         }
         let content = access(&service, "text/plain", "http://127.0.0.1:7979/").await;
-        assert!(content.contains("test1.txt") && content.contains("test2.txt"));
+        assert!(content.contains("Index page"));
 
-        let content = access(&service, "text/xml", "http://127.0.0.1:7979/").await;
-        assert!(content.starts_with("<list>") && content.contains("test1.txt") && content.contains("test2.txt"));
+        let content = access(&service, "text/plain", "http://127.0.0.1:7979/dir1/").await;
+        assert!(content.contains("test3.txt") && content.contains("dir2"));
 
-        let content = access(&service, "text/html", "http://127.0.0.1:7979/").await;
-        assert!(content.contains("<html>") && content.contains("test1.txt") && content.contains("test2.txt"));
+        let content = access(&service, "text/xml", "http://127.0.0.1:7979/dir1/").await;
+        assert!(content.starts_with("<list>") && content.contains("test3.txt") && content.contains("dir2"));
 
-        let content = access(&service, "application/json", "http://127.0.0.1:7979/").await;
-        assert!(content.starts_with('{') && content.contains("test1.txt") && content.contains("test2.txt"));
+        let content = access(&service, "text/html", "http://127.0.0.1:7979/dir1/").await;
+        assert!(content.contains("<html>") && content.contains("test3.txt") && content.contains("dir2"));
+
+        let content = access(&service, "application/json", "http://127.0.0.1:7979/dir1/").await;
+        assert!(content.starts_with('{') && content.contains("test3.txt") && content.contains("dir2"));
 
         let content = access(&service, "text/plain", "http://127.0.0.1:7979/test1.txt").await;
         assert!(content.contains("copy1"));
@@ -103,5 +108,47 @@ mod tests {
         )
         .await;
         assert!(content == "copy3");
+    }
+
+    #[tokio::test]
+    async fn test_serve_static_file() {
+        let router =
+            Router::with_path("test1.txt").get(StaticFile::new("../examples/static-dir-list/static/test/test1.txt"));
+
+        let mut response = TestClient::get("http://127.0.0.1:7979/test1.txt").send(router).await;
+        assert_eq!(response.status_code().unwrap(), StatusCode::OK);
+        assert_eq!(response.take_string().await.unwrap(), "copy1");
+    }
+
+    #[tokio::test]
+    async fn test_serve_embed_files() {
+        #[derive(RustEmbed)]
+        #[folder = "test/static"]
+        struct Assets;
+
+        let router = Router::new()
+            .push(Router::with_path("files/<**path>").get(serve_file))
+            .push(Router::with_path("dir/<**path>").get(static_embed::<Assets>().with_fallback("index.html")));
+        let service = Service::new(router);
+
+        #[handler]
+        async fn serve_file(req: &mut Request, res: &mut Response) {
+            let path = req.param::<String>("**path").unwrap();
+            if let Some(file) = Assets::get(&path) {
+                file.render(req, res);
+            }
+        }
+
+        let mut response = TestClient::get("http://127.0.0.1:7979/files/test1.txt")
+            .send(&service)
+            .await;
+        assert_eq!(response.status_code().unwrap(), StatusCode::OK);
+        assert_eq!(response.take_string().await.unwrap(), "copy1");
+
+        let mut response = TestClient::get("http://127.0.0.1:7979/dir/test1.txt")
+            .send(&service)
+            .await;
+        assert_eq!(response.status_code().unwrap(), StatusCode::OK);
+        assert_eq!(response.take_string().await.unwrap(), "copy1");
     }
 }
