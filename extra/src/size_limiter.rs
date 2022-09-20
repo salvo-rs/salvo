bbeb861e55e7f9fdd6a41d462645ce69bea3c890
Chrislearn Young <chris@acroidea.com>
2022-09-20 08:23:15 +0800

    Update size_limiter.rs

--- extra/src/size_limiter.rs
+++ extra/src/size_limiter.rs
@@ -1,9 +1,9 @@
 //! size limiter middleware
 
-use salvo_core::hyper::body::HttpBody;
 use salvo_core::http::StatusError;
 use salvo_core::http::{Request, Response};
-use salvo_core::{async_trait, Depot, Handler, FlowCtrl};
+use salvo_core::hyper::body::HttpBody;
+use salvo_core::{async_trait, Depot, FlowCtrl, Handler};
 
 /// MaxSize
 pub struct MaxSize(pub u64);
@@ -19,7 +19,7 @@ impl Handler for MaxSize {
                 ctrl.call_next(req, depot, res).await;
             }
         } else {
-            res.set_status_error(StatusError::bad_request().with_detial("body size is unknown"));
+            res.set_status_error(StatusError::bad_request().with_detail("body size is unknown"));
             ctrl.skip_rest();
         }
     }
