2696a5631823ff4072633f1b74cb03e549e2aeb5
Chrislearn Young <chris@acroidea.com>
2022-09-28 15:51:04 +0800

    remove doc

--- crates/extra/src/logging.rs
+++ crates/extra/src/logging.rs
@@ -1,24 +1,4 @@
 //! Logging middleware
-//! 
-//! #Example
-//! ```
-//! use salvo::extra::logging::Logger;
-//! use salvo::prelude::*;
-//! 
-//! #[handler]
-//! async fn hello_world() -> &'static str {
-//!     "Hello World"
-//! }
-//! 
-//! #[tokio::main]
-//! async fn main() {
-//!     tracing_subscriber::fmt().init();
-//! 
-//!     let router = Router::new().hoop(Logger).get(hello_world);
-//!     tracing::info!("Listening on http://127.0.0.1:7878");
-//!     Server::new(TcpListener::bind("127.0.0.1:7878")).serve(router).await;
-//! }
-//! ```
 use std::time::Instant;
 
 use tracing::{Instrument, Level};
