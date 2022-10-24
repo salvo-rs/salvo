ada1c99e05b1db293cf563ce0111e5e983959520
Chrislearn Young <chris@acroidea.com>
2022-10-24 18:03:35 +0800

    🚧 PROGRESS: Hyper 1.0 and Http3

--- examples/hello-h3/src/main.rs
+++ examples/hello-h3/src/main.rs
@@ -6,99 +6,48 @@ use rustls::{Certificate, PrivateKey};
 use structopt::StructOpt;
 use tokio::{fs::File, io::AsyncReadExt};
 
+use salvo::conn::rustls::{Keycert, RustlsConfig};
 use salvo::prelude::*;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "server")]
-struct Opt {
-    #[structopt(
-        name = "dir",
-        short,
-        long,
-        help = "Root directory of the files to serve. \
-                If omitted, server will respond OK."
-    )]
-    pub root: Option<PathBuf>,
-
-    #[structopt(
-        short,
-        long,
-        default_value = "127.0.0.1:7878",
-        help = "What address:port to listen for new connections"
-    )]
-    pub listen: SocketAddr,
-
-    #[structopt(flatten)]
-    pub certs: Certs,
-}
-
-#[derive(StructOpt, Debug)]
-pub struct Certs {
-    #[structopt(
-        long,
-        short,
-        help = "Certificate for TLS. \
-                If present, `--key` is mandatory. \
-                If omitted, a selfsigned certificate will be generated."
-    )]
-    pub cert: Option<PathBuf>,
-
-    #[structopt(long, short, help = "Private key for the certificate.")]
-    pub key: Option<PathBuf>,
-}
-
 #[handler]
-async fn hello() -> &'static str {
+async fn hello(res: &mut Response) -> &'static str {
+    res.add_header(
+        "alt-svc",
+        r#"h3-29=":7878"; ma=2592000,quic=":7878"; ma=2592000; v="46,43""#,
+        true,
+    );
     "Hello World"
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
-    let opt = Opt::from_args();
-    let crypto = load_crypto(opt.certs).await.unwrap();
-    let server_config = salvo::conn::quic::ServerConfig::with_crypto(Arc::new(crypto));
+    let (cert, key) = build_certs();
 
     let router = Router::new().get(hello);
-    Server::new(QuicListener::bind(("127.0.0.1", 7878), server_config)).serve(router).await;
-}
-
-static ALPN: &[u8] = b"h3";
-
-async fn load_crypto(opt: Certs) -> Result<rustls::ServerConfig, Box<dyn std::error::Error>> {
-    let (cert, key) = match (opt.cert, opt.key) {
-        (None, None) => build_certs(),
-        (Some(cert_path), Some(ref key_path)) => {
-            let mut cert_v = Vec::new();
-            let mut key_v = Vec::new();
-
-            let mut cert_f = File::open(cert_path).await?;
-            let mut key_f = File::open(key_path).await?;
-
-            cert_f.read_to_end(&mut cert_v).await?;
-            key_f.read_to_end(&mut key_v).await?;
-            (rustls::Certificate(cert_v), PrivateKey(key_v))
-        }
-        (_, _) => return Err("cert and key args are mutually dependant".into()),
-    };
-
+    let config = RustlsConfig::new(Keycert::new().with_cert(cert.as_slice()).with_key(key.as_slice()));
+    let listener = RustlsListener::bind(config, "127.0.0.1:7878");
+    
+    let cert = Certificate(cert);
+    let key = PrivateKey(key);
     let mut crypto = rustls::ServerConfig::builder()
         .with_safe_default_cipher_suites()
         .with_safe_default_kx_groups()
         .with_protocol_versions(&[&rustls::version::TLS13])
         .unwrap()
         .with_no_client_auth()
-        .with_single_cert(vec![cert], key)?;
+        .with_single_cert(vec![cert], key).unwrap();
     crypto.max_early_data_size = u32::MAX;
     crypto.alpn_protocols = vec![ALPN.into()];
+    let server_config = salvo::conn::quic::ServerConfig::with_crypto(Arc::new(crypto));
+    let listener = QuicListener::bind(("127.0.0.1", 7878), server_config).join(listener);
 
-    Ok(crypto)
+    Server::new(listener).serve(router).await;
 }
 
-pub fn build_certs() -> (Certificate, PrivateKey) {
+static ALPN: &[u8] = b"h3";
+
+pub fn build_certs() -> (Vec<u8>, Vec<u8>) {
     let cert = rcgen::generate_simple_self_signed(vec!["127.0.0.1".into()]).unwrap();
-    let key = PrivateKey(cert.serialize_private_key_der());
-    let cert = Certificate(cert.serialize_der().unwrap());
-    println!("============================3");
-    (cert, key)
+    (cert.serialize_der().unwrap(), cert.serialize_private_key_der())
 }
