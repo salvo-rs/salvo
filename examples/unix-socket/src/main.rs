2fe63a2fc26926c678e19d85304307ca7ace0e2a
Chrislearn Young <chris@acroidea.com>
2022-10-26 18:29:30 +0800

    Fix example error

--- examples/unix-socket/src/main.rs
+++ examples/unix-socket/src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     use salvo_static::StaticDir;
 
     let router = Router::with_path("files/<*path>").get(StaticDir::new("./static"));
-    Server::new(UnixListener::bind("/tmp/salvo.sock")).serve(router).await;
+    Server::new(UnixListener::bind("/tmp/salvo.sock")).await.serve(router).await;
 }
 
 #[cfg(not(target_os = "linux"))]
