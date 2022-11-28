c31890d8fbe2e70a5028d44f1a817e6e11978465
Chrislearn Young <chris@acroidea.com>
2022-11-27 19:26:36 -0500

    update hello

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -5,8 +5,8 @@ async fn hello() -> &'static str {
     "Hello World"
 }
 #[handler]
-async fn hello1() -> Result<&'static str, ()> {
-    Ok("Hello World1")
+async fn hello_zh() -> Result<&'static str, ()> {
+    Ok("你好，世界！")
 }
 #[handler]
 async fn hello2(res: &mut Response) {
@@ -28,7 +28,7 @@ async fn main() {
 fn route() -> Router {
     Router::new()
         .get(hello)
-        .push(Router::with_path("hello1").get(hello1))
+        .push(Router::with_path("你好").get(hello_zh))
         .push(Router::with_path("hello2").get(hello2))
         .push(Router::with_path("hello3").get(hello3))
 }
