e29cc832e00b2bd9215836feb5306ccc141c737b
Chrislearn Young <chris@acroidea.com>
2022-12-22 16:58:37 +0800

    unit test for macos

--- crates/core/src/conn/native_tls/mod.rs
+++ crates/core/src/conn/native_tls/mod.rs
@@ -11,17 +11,22 @@ mod tests {
     use tokio::net::TcpStream;
 
     use super::*;
-    use crate::conn::{Accepted, Acceptor, Listener, TcpListener};
+    use crate::conn::{TcpListener, Accepted,Acceptor, Listener};
 
     #[tokio::test]
     async fn test_native_tls_listener() {
-        #[cfg(target_os = "macos")]
-        let identity: Vec<u8> = include_bytes!("../../../certs/identity-legacy.p12").to_vec();
-        #[cfg(not(target_os = "macos"))]
-        let identity: Vec<u8> = include_bytes!("../../../certs/identity.p12").to_vec();
+        let identity = if cfg!(target_os = "macos") {
+            include_bytes!("../../../certs/identity-legacy.p12").to_vec()
+        } else {
+            include_bytes!("../../../certs/identity.p12").to_vec()
+        };
 
         let mut acceptor = TcpListener::new("127.0.0.1:0")
-            .native_tls(NativeTlsConfig::new().with_pkcs12(identity).with_password("mypass"))
+            .native_tls(
+                NativeTlsConfig::new()
+                    .with_pkcs12(identity)
+                    .with_password("mypass"),
+            )
             .bind()
             .await;
         let addr = acceptor.holdings()[0].local_addr.clone().into_std().unwrap();
