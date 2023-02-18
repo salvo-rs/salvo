52b9abc85fdd2a80f51e8b7ca34e108305359c54
Chrislearn Young <chris@acroidea.com>
2023-02-18 23:49:54 +0800

    wip for hyper 1.0

--- examples/work-with-tower/src/main.rs
+++ examples/work-with-tower/src/main.rs
@@ -1,5 +1,10 @@
+
+use std::net::SocketAddr;
+
 use salvo::prelude::*;
 use tower::limit::ConcurrencyLimit;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
 
 #[handler]
 async fn hello() -> &'static str {
@@ -13,8 +18,16 @@ async fn main() {
     let router = Router::new().get(hello);
     let server = ConcurrencyLimit::new(Service::new(router), 20);
 
-    
-    let _ = hyper::server::Server::bind(&std::net::SocketAddr::from(([127, 0, 0, 1], 7878)))
-        .serve(server)
-        .await;
+    let addr: SocketAddr = ([127, 0, 0, 1], 7878).into();
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new().serve_connection(stream, server).await {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
