c1e5bb134878df6ce7da7103a9753d7dfe36adb0
Chrislearn Young <chris@acroidea.com>
2023-02-20 17:29:01 +0800

    Update main.rs

--- examples/logging/src/main.rs
+++ examples/logging/src/main.rs
@@ -10,7 +10,7 @@ async fn hello() -> &'static str {
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new().hoop(Logger).get(hello);
+    let router = Router::new().hoop(Logger::new()).get(hello);
 
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor).serve(router).await;
