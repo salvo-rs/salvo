70aff1bbc042e1674c91c652f3c6854fde9c6bc4
Chrislearn Young <chris@acroidea.com>
2023-07-01 16:06:59 +0800

    logging middleware: res.status_code is empty #305

--- crates/extra/src/logging.rs
+++ crates/extra/src/logging.rs
@@ -34,16 +34,7 @@ impl Handler for Logger {
             ctrl.call_next(req, depot, res).await;
             let duration = now.elapsed();
 
-            let status = match res.status_code {
-                Some(code) => code,
-                None => {
-                    if res.body.is_none() {
-                        StatusCode::NOT_FOUND
-                    } else {
-                        StatusCode::OK
-                    }
-                }
-            };
+            let status = match res.status_code.unwrap_or(StatusCode::OK);
             tracing::info!(
                 status = %status,
                 duration = ?duration,
