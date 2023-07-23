5b43f1425f33301ed273f84a9f32cafd2a9fefb5
Chrislearn Young <chris@acroidea.com>
2023-07-24 04:04:12 +0800

    chore!(extra): tokio-tungstenite upgrade to 0.20 (#334)
    * chore!(extra): tokio-tungstenite upgrade to 0.20
    
    * wip

--- crates/extra/src/websocket.rs
+++ crates/extra/src/websocket.rs
@@ -62,14 +62,37 @@ impl WebSocketUpgrade {
         WebSocketUpgrade { config: Some(config) }
     }
 
-    /// Sets the size of the internal message send queue.
+    /// The target minimum size of the write buffer to reach before writing the data
+    /// to the underlying stream.
+    /// The default value is 128 KiB.
+    ///
+    /// If set to `0` each message will be eagerly written to the underlying stream.
+    /// It is often more optimal to allow them to buffer a little, hence the default value.
     #[inline]
-    pub fn max_send_queue(mut self, max: usize) -> Self {
-        self.config.get_or_insert_with(WebSocketConfig::default).max_send_queue = Some(max);
+    pub fn write_buffer_size(mut self, max: usize) -> Self {
+        self.config.get_or_insert_with(WebSocketConfig::default).write_buffer_size = max;
         self
     }
 
-    /// Sets the maximum message size (defaults to 64 megabytes)
+    /// The max size of the write buffer in bytes. Setting this can provide backpressure
+    /// in the case the write buffer is filling up due to write errors.
+    /// The default value is unlimited.
+    ///
+    /// Note: The write buffer only builds up past [`write_buffer_size`](Self::write_buffer_size)
+    /// when writes to the underlying stream are failing. So the **write buffer can not
+    /// fill up if you are not observing write errors even if not flushing**.
+    ///
+    /// Note: Should always be at least [`write_buffer_size + 1 message`](Self::write_buffer_size)
+    /// and probably a little more depending on error handling strategy.
+    #[inline]
+    pub fn max_write_buffer_size(mut self, max: usize) -> Self {
+        self.config.get_or_insert_with(WebSocketConfig::default).max_write_buffer_size = max;
+        self
+    }
+
+    /// The maximum size of a message. `None` means no size limit. The default value is 64 MiB
+    /// which should be reasonably big for all normal use-cases but small enough to prevent
+    /// memory eating by a malicious user.
     #[inline]
     pub fn max_message_size(mut self, max: usize) -> Self {
         self.config
@@ -78,12 +101,27 @@ impl WebSocketUpgrade {
         self
     }
 
-    /// Sets the maximum frame size (defaults to 16 megabytes)
+    /// The maximum size of a single message frame. `None` means no size limit. The limit is for
+    /// frame payload NOT including the frame header. The default value is 16 MiB which should
+    /// be reasonably big for all normal use-cases but small enough to prevent memory eating
+    /// by a malicious user.
     #[inline]
     pub fn max_frame_size(mut self, max: usize) -> Self {
         self.config.get_or_insert_with(WebSocketConfig::default).max_frame_size = Some(max);
         self
     }
+
+    /// When set to `true`, the server will accept and handle unmasked frames
+    /// from the client. According to the RFC 6455, the server must close the
+    /// connection to the client in such cases, however it seems like there are
+    /// some popular libraries that are sending unmasked frames, ignoring the RFC.
+    /// By default this option is set to `false`, i.e. according to RFC 6455.
+    #[inline]
+    pub fn accept_unmasked_frames(mut self, accept: bool) -> Self {
+        self.config.get_or_insert_with(WebSocketConfig::default).accept_unmasked_frames = accept;
+        self
+    }
+
 
     /// Upgrade websocket request.
     #[inline]
