297346b8d89ad4cf21b1022cc0556748f982d633
Christopher Young <chris@acroidea.com>
2023-08-15 07:01:04 +0800

    fix(example): proxy path issue

--- examples/proxy-simple/src/main.rs
+++ examples/proxy-simple/src/main.rs
@@ -6,8 +6,7 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     let router = Router::new()
-        .push(Router::with_path("google/<**rest>").handle(Proxy::new(["https://www.google.com"])))
-        .push(Router::with_path("rust/<**rest>").handle(Proxy::new("https://www.rust-lang.org")));
+        .push(Router::with_path("<**rest>").handle(Proxy::new("https://www.rust-lang.org")));
 
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor).serve(router).await;
