8ea88cf10f3bb3a9244ee389ec9c2c6bea438f31
Christopher Young <chris@acroidea.com>
2023-09-16 05:49:30 +0800

    update hello

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -8,29 +8,13 @@ async fn hello() -> &'static str {
 async fn hello_zh() -> Result<&'static str, ()> {
     Ok("你好，世界！")
 }
-#[handler]
-async fn hello2(res: &mut Response) {
-    res.render("Hello World2");
-}
-#[handler]
-async fn hello3(_req: &mut Request, res: &mut Response) {
-    res.render(Text::Plain("Hello World3"));
-}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
     let acceptor = TcpListener::new("0.0.0.0:5800").bind().await;
-    let router = route();
+    let router = Router::new().get(hello).push(Router::with_path("你好").get(hello_zh));
     println!("{:?}", router);
     Server::new(acceptor).serve(router).await;
-}
-
-fn route() -> Router {
-    Router::new()
-        .get(hello)
-        .push(Router::with_path("a").get(hello2))
-        .push(Router::with_path("你好").get(hello_zh))
-        .push(Router::with_path("hello3").get(hello3))
 }
