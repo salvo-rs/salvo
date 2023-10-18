083b418f5e3d15135be11da61b47b719fd2a7d70
Chrislearn Young <chris@acroidea.com>
2023-10-18 13:07:43 +0800

    Allow openapi UIs setting `lib_url`

--- crates/oapi/src/swagger_ui/mod.rs
+++ crates/oapi/src/swagger_ui/mod.rs
@@ -27,8 +27,6 @@ const INDEX_TMPL: &str = r#"
     <meta charset="UTF-8">
     <title>Swagger UI</title>
     <link rel="stylesheet" type="text/css" href="./swagger-ui.css" />
-    <link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" />
     <style>
     html {
         box-sizing: border-box;
