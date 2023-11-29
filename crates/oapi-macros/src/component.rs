482dc60d046965c595ed22e0b3062ad40d2dfe40
Chrislearn Young <chris@acroidea.com>
2023-11-29 18:13:26 +0800

    oapi: Fix `additionalProperties` not working for `Object`

--- crates/oapi-macros/src/component.rs
+++ crates/oapi-macros/src/component.rs
@@ -336,10 +336,7 @@ impl<'c> ComponentSchema {
                 let is_inline = features.is_inline();
 
                 if type_tree.is_object() {
-                    tokens.extend(quote! {
-                        #oapi::oapi::Object::new()
-                            #description_stream #deprecated_stream #nullable
-                    })
+                    ComponentSchema::map_to_tokens(tokens, features, type_tree, object_name, description_stream, deprecated_stream, type_definition);
                 } else {
                     let type_path = &**type_tree.path.as_ref().unwrap();
                     let schema = if type_definition {
