a70dcee6d2e3f6406914991b1644818c819b7670
Aston Turing <aston@sonc.ai>
2023-12-05 17:57:19 +0800

    Update swagger_ui to v5.10.3

--- crates/oapi/src/swagger_ui/mod.rs
+++ crates/oapi/src/swagger_ui/mod.rs
@@ -17,7 +17,7 @@ use salvo_core::{async_trait, Depot, Error, FlowCtrl, Handler, Request, Response
 use serde::Serialize;
 
 #[derive(RustEmbed)]
-#[folder = "src/swagger_ui/v5.9.0"]
+#[folder = "src/swagger_ui/v5.10.3"]
 struct SwaggerUiDist;
 
 const INDEX_TMPL: &str = r#"
