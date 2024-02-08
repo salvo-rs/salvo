3777c166a47a882e499327a9d45c0bbc5b1b9275
Chrislearn Young <chris@acroidea.com>
2024-02-08 20:08:32 +0800

    Lint configuration through Cargo (#682)

--- crates/cache/src/lib.rs
+++ crates/cache/src/lib.rs
@@ -14,9 +14,6 @@
 #![doc(html_favicon_url = "https://salvo.rs/favicon-32x32.png")]
 #![doc(html_logo_url = "https://salvo.rs/images/logo.svg")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![deny(unreachable_pub)]
-#![forbid(unsafe_code)]
-#![warn(missing_docs)]
 
 use std::borrow::Borrow;
 use std::collections::VecDeque;
