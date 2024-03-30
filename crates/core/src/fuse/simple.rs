62b08329f48cbd96ca3dc102a64162d459d51df7
Chrislearn Young <chris@acroidea.com>
2024-03-30 09:47:36 +0800

    wip

--- crates/core/src/fuse/simple.rs
+++ crates/core/src/fuse/simple.rs
@@ -8,9 +8,44 @@ use tokio_util::sync::CancellationToken;
 
 use super::{async_trait, ArcFusewire, FuseEvent, FuseFactory, FuseInfo, Fusewire};
 
+
+/// A guard action.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum GuardAction {
+    /// Reject the connection.
+    Reject,
+    /// Allow the event to next guards.
+    ToNext,
+    /// Permit the event and skip next guards.
+    Permit,
+}
+/// A guard.
+pub trait Guard: Sync + Send + 'static{
+    /// Check the event.
+    fn check(&self, info: &FuseInfo, event: &FuseEvent) -> GuardAction;
+}
+impl<F> Guard for F where F: Fn(&FuseInfo, &FuseEvent) -> GuardAction + Sync + Send + 'static{
+    fn check(&self, info: &FuseInfo, event: &FuseEvent) -> GuardAction {
+        self(info, event)
+    }
+}
+
+/// Skip the quic connection.
+pub fn skip_quic(info: &FuseInfo, _event: &FuseEvent) -> GuardAction {
+    if info.trans_proto.is_quic() {
+        GuardAction::Permit
+    } else {
+        GuardAction::ToNext
+    }
+}
+
+
 /// A simple fusewire.
 pub struct SimpleFusewire {
     info: FuseInfo,
+    guards: Arc<Vec<Box<dyn Guard>>>,
+
+    reject_token: CancellationToken,
 
     tcp_idle_timeout: Duration,
     tcp_idle_token: CancellationToken,
@@ -24,7 +59,6 @@ pub struct SimpleFusewire {
     tls_handshake_token: CancellationToken,
     tls_handshake_notify: Arc<Notify>,
 }
-
 impl SimpleFusewire {
     /// Create a new `SimpleFusewire`.
     pub fn new(info: FuseInfo) -> Self {
@@ -51,8 +85,17 @@ impl SimpleFusewire {
 #[async_trait]
 impl Fusewire for SimpleFusewire {
     fn event(&self, event: FuseEvent) {
-        if self.info.trans_proto.is_quic() {
-            return;
+        for guard in self.guards.iter() {
+            match guard.check(&self.info, &event){
+                GuardAction::Permit => {
+                    return;
+                }
+                GuardAction::Reject => {
+                    self.reject_token.cancel();
+                    return;
+                }
+                _ => {}
+            }
         }
         self.tcp_idle_notify.notify_waiters();
         match event {
@@ -96,6 +139,7 @@ impl Fusewire for SimpleFusewire {
     }
     async fn fused(&self) {
         tokio::select! {
+            _ = self.reject_token.cancelled() => {}
             _ = self.tcp_idle_token.cancelled() => {}
             _ = self.tcp_frame_token.cancelled() => {}
             _ = self.tls_handshake_token.cancelled() => {}
@@ -104,11 +148,13 @@ impl Fusewire for SimpleFusewire {
 }
 
 /// A [`SimpleFusewire`] builder.
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct SimpleFactory {
     tcp_idle_timeout: Duration,
     tcp_frame_timeout: Duration,
     tls_handshake_timeout: Duration,
+
+    guards: Arc<Vec<Box<dyn Guard>>>,
 }
 
 impl Default for SimpleFactory {
@@ -124,6 +170,7 @@ impl SimpleFactory {
             tcp_idle_timeout: Duration::from_secs(30),
             tcp_frame_timeout: Duration::from_secs(60),
             tls_handshake_timeout: Duration::from_secs(10),
+            guards: Arc::new(vec![Box::new(skip_quic)]),
         }
     }
 
@@ -138,12 +185,24 @@ impl SimpleFactory {
         self
     }
 
+    /// Set guards to new value.
+    pub fn guards(mut self, guards: Vec<Box<dyn Guard>>) -> Self {
+        self.guards = Arc::new(guards);
+        self
+    }
+    /// Add a guard.
+    pub fn add_guard(mut self, guard: impl Guard) -> Self {
+        Arc::get_mut(&mut self.guards).expect("guards get mut failed").push(Box::new(guard));
+        self
+    }
+
     /// Build a `SimpleFusewire`.
     pub fn build(&self, info: FuseInfo) -> SimpleFusewire {
         let Self {
             tcp_idle_timeout,
             tcp_frame_timeout,
             tls_handshake_timeout,
+            guards,
         } = self.clone();
 
         let tcp_idle_token = CancellationToken::new();
@@ -165,6 +224,9 @@ impl SimpleFactory {
         });
         SimpleFusewire {
             info,
+            guards,
+
+            reject_token: CancellationToken::new(),
 
             tcp_idle_timeout,
             tcp_idle_token,
