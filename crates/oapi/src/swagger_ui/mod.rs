ed4839971ab2b082a78b522b2fa8dfd40186a0e2
Chrislearn Young <chris@acroidea.com>
2024-05-25 06:20:24 +0800

    Update swggerUI to 5.17.12

--- crates/oapi/src/swagger_ui/mod.rs
+++ crates/oapi/src/swagger_ui/mod.rs
@@ -18,7 +18,7 @@ use salvo_core::{async_trait, Depot, Error, FlowCtrl, Handler, Request, Response
 use serde::Serialize;
 
 #[derive(RustEmbed)]
-#[folder = "src/swagger_ui/v5.13.0"]
+#[folder = "src/swagger_ui/v5.17.12"]
 struct SwaggerUiDist;
 
 const INDEX_TMPL: &str = r#"
