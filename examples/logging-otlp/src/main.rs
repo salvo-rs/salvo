820160fa03ecccb20e101dd2a8c74fb527e3f30c
Fankai Liu <liufankai137@outlook.com>
2024-05-25 21:02:56 +0800

    add logging-otlp example

---
+++ examples/logging-otlp/src/main.rs
@@ -0,0 +1,76 @@
+use anyhow::Result;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{self, RandomIdGenerator, Tracer},
+    Resource,
+};
+use salvo::logging::Logger;
+use salvo::prelude::*;
+use tracing::{debug, info, instrument, level_filters::LevelFilter, warn};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
+
+#[instrument(fields(http.uri = req.uri().path(), http.method = req.method().as_str()))]
+#[handler]
+async fn hello(req: &mut Request) -> &'static str {
+    "Hello World"
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // console layer for tracing-subscriber
+    let console = fmt::Layer::new()
+        .with_span_events(FmtSpan::CLOSE)
+        .pretty()
+        .with_filter(LevelFilter::DEBUG);
+
+    // file appender layer for tracing-subscriber
+    let file_appender = tracing_appender::rolling::daily("./logs", "salvo.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let file = fmt::Layer::new()
+        .with_writer(non_blocking)
+        .pretty()
+        .with_filter(LevelFilter::INFO);
+
+    // opentelemetry tracing layer for tracing-subscriber
+    let tracer = init_tracer()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
+        .with(console)
+        .with(file)
+        .with(opentelemetry)
+        .init();
+
+    let router = Router::new().get(hello);
+    let service = Service::new(router).hoop(Logger::new());
+
+    let acceptor = TcpListener::new("0.0.0.0:5800").bind().await;
+    Server::new(acceptor).serve(service).await;
+    Ok(())
+}
+
+fn init_tracer() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            trace::config()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(32)
+                .with_max_attributes_per_span(64)
+                .with_resource(Resource::new(vec![KeyValue::new("service.name", "salvo-tracing")])),
+        )
+        .install_batch(runtime::Tokio)?;
+    Ok(tracer)
+}
