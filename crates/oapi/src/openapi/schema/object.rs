0993f81ed4f273a0c6fb1320b4fed0c225e09f10
Chrislearn Young <chris@acroidea.com>
2024-06-05 14:39:59 +0800

    oapi: Add preserve-order features (#803)
    * oapi: Add `preserve-order` features
    
    * Format Rust code using rustfmt
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- crates/oapi/src/openapi/schema/object.rs
+++ crates/oapi/src/openapi/schema/object.rs
@@ -1,16 +1,9 @@
-use std::collections::BTreeMap;
-
 use indexmap::IndexSet;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 use super::AdditionalProperties;
-use crate::{Deprecated, RefOr, Schema, SchemaFormat, SchemaType, ToArray, Xml};
-
-#[cfg(not(feature = "preserve_order"))]
-type ObjectPropertiesMap<K, V> = BTreeMap<K, V>;
-#[cfg(feature = "preserve_order")]
-type ObjectPropertiesMap<K, V> = indexmap::IndexMap<K, V>;
+use crate::{Deprecated, PropMap, RefOr, Schema, SchemaFormat, SchemaType, ToArray, Xml};
 
 /// Implements subset of [OpenAPI Schema Object][schema] which allows
 /// adding other [`Schema`]s as **properties** to this [`Schema`].
@@ -56,16 +49,13 @@ pub struct Object {
 
     /// Map of fields with their [`Schema`] types.
     ///
-    /// With **preserve_order** feature flag [`indexmap::IndexMap`] will be used as
+    /// With **preserve-order** feature flag [`indexmap::IndexMap`] will be used as
     /// properties map backing implementation to retain property order of [`ToSchema`][to_schema].
     /// By default [`BTreeMap`] will be used.
     ///
     /// [to_schema]: crate::ToSchema
-    #[serde(
-        skip_serializing_if = "ObjectPropertiesMap::is_empty",
-        default = "ObjectPropertiesMap::new"
-    )]
-    pub properties: ObjectPropertiesMap<String, RefOr<Schema>>,
+    #[serde(skip_serializing_if = "PropMap::is_empty", default = "PropMap::new")]
+    pub properties: PropMap<String, RefOr<Schema>>,
 
     /// Additional [`Schema`] for non specified fields (Useful for typed maps).
     #[serde(skip_serializing_if = "Option::is_none")]
