613d5fbdb0120c05fd103631d07039e1de952cfd
Chrislearn Young <chris@acroidea.com>
2024-06-09 06:55:24 +0800

    fix

--- crates/core/src/routing/filters/others.rs
+++ crates/core/src/routing/filters/others.rs
@@ -16,7 +16,9 @@ impl MethodFilter {
 impl Filter for MethodFilter {
     #[inline]
     fn filter(&self, req: &mut Request, state: &mut PathState) -> bool {
-        state.has_any_goal = true;
+        if state.is_ended() {
+            state.has_any_goal = true;
+        }
         req.method() == self.0
     }
 }
