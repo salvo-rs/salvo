525aa2ff7b87e7033a9d05142e99a005a4bc5f30
阿豪 <504595380@qq.com>
2024-06-13 09:59:21 +0800

    fix: html css style (#810)

--- crates/serve-static/src/dir.rs
+++ crates/serve-static/src/dir.rs
@@ -629,7 +629,7 @@ const HTML_STYLE: &str = r#"
             --link-visited-color: #f25555;
             --dir-icon-color: #7da3d0;
             --file-icon-color: #545d68;
-        }}
+        }
     }"#;
 const DIR_ICON: &str = r#"<svg aria-label="Directory" data-icon="dir" width="20" height="20" viewBox="0 0 512 512" version="1.1" role="img"><path fill="currentColor" d="M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"></path></svg>"#;
 const FILE_ICON: &str = r#"<svg aria-label="File" data-icon="file" width="20" height="20" viewBox="0 0 384 512" version="1.1" role="img"><path d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48z"/></svg>"#;
