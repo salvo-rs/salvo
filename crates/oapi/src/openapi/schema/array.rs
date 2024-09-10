577bb8354c34aaa48a64bfebb7e9de58245b0ae6
Chrislearn Young <chris@acroidea.com>
2024-09-11 06:49:56 +0800

    fix ci and compile error

--- crates/oapi/src/openapi/schema/array.rs
+++ crates/oapi/src/openapi/schema/array.rs
@@ -271,9 +271,9 @@ mod tests {
     #[test]
     fn test_array_with_extensions() {
         let expected = json!("value");
-        let json_value = Array::default().extensions(Some(
+        let json_value = Array::default().extensions(
             [("x-some-extension".to_string(), expected.clone())].into(),
-        ));
+        );
 
         let value = serde_json::to_value(&json_value).unwrap();
         assert_eq!(value.get("x-some-extension"), Some(&expected));
