2a121ef14fbc9280224aa2060bca1696691dc124
Chrislearn Young <chris@acroidea.com>
2024-09-23 21:33:27 +0800

    update craft example

--- crates/craft-macros/examples/example-add.rs
+++ crates/craft-macros/examples/example-add.rs
@@ -5,21 +5,6 @@ use salvo::prelude::*;
 use salvo_craft_macros::craft;
 use std::sync::Arc;
 
-#[tokio::main]
-async fn main() {
-    let opts = Arc::new(Opts::new(1));
-    let router = Router::new()
-        .push(Router::with_path("add1").get(opts.add1()))
-        .push(Router::with_path("add2").get(opts.add2()))
-        .push(Router::with_path("add3").get(Opts::add3()));
-    let doc = OpenApi::new("Example API", "0.0.1").merge_router(&router);
-    let router = router
-        .push(doc.into_router("/api-doc/openapi.json"))
-        .push(SwaggerUi::new("/api-doc/openapi.json").into_router("swagger-ui"));
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
-    Server::new(acceptor).serve(router).await;
-}
-
 #[derive(Clone)]
 pub struct Opts {
     state: i64,
@@ -52,4 +37,19 @@ impl Opts {
     pub fn add3(left: QueryParam<i64>, right: QueryParam<i64>) -> String {
         (*left + *right).to_string()
     }
+}
+
+#[tokio::main]
+async fn main() {
+    let opts = Arc::new(Opts::new(1));
+    let router = Router::new()
+        .push(Router::with_path("add1").get(opts.add1()))
+        .push(Router::with_path("add2").get(opts.add2()))
+        .push(Router::with_path("add3").get(Opts::add3()));
+    let doc = OpenApi::new("Example API", "0.0.1").merge_router(&router);
+    let router = router
+        .push(doc.into_router("/api-doc/openapi.json"))
+        .push(SwaggerUi::new("/api-doc/openapi.json").into_router("swagger-ui"));
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
 }
