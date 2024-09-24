2c1da206b58cf3f57966abd5682e375863b364d4
Chrislearn Young <chris@acroidea.com>
2024-09-24 23:44:09 +0800

    Refactor Path Filter and CombWisp (#928)
    * Refactor Path Filter and CombWisp
    
    * cargo clippy
    
    * wip
    
    * wip
    
    * wip
    
    * Format Rust code using rustfmt
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- crates/core/src/routing/mod.rs
+++ crates/core/src/routing/mod.rs
@@ -428,15 +428,22 @@ impl PathParams {
                 panic!("only one wildcard param is allowed and it must be the last one.");
             }
         }
-        if name.starts_with("*+") || name.starts_with("*?") || name.starts_with("**") {
-            self.inner.insert(name[2..].to_owned(), value);
-            self.greedy = true;
-        } else if let Some(name) = name.strip_prefix('*') {
-            self.inner.insert(name.to_owned(), value);
+        if name.starts_with('*') {
+            self.inner.insert(split_wild_name(name).1.to_owned(), value);
             self.greedy = true;
         } else {
             self.inner.insert(name.to_owned(), value);
         }
+    }
+}
+
+pub(crate) fn split_wild_name(name: &str) -> (&str, &str) {
+    if name.starts_with("*+") || name.starts_with("*?") || name.starts_with("**") {
+        (&name[0..2], &name[2..])
+    } else if let Some(stripped) = name.strip_prefix('*') {
+        ("*", stripped)
+    } else {
+        ("", name)
     }
 }
 
