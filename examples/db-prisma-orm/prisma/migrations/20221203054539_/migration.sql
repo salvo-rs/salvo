6ab6946047030508ed2369f86b38148bd3d83257
Chrislearn Young <chris@acroidea.com>
2025-03-28 15:52:10 +0800

    Remove example db-prisma-orm

--- examples/db-prisma-orm/prisma/migrations/20221203054539_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "username" TEXT NOT NULL,
-    "email" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
--- examples/db-prisma-orm/src/bin/prisma.rs
+++
@@ -1,4 +0,0 @@
-// Entry point for Prisma CLI integration
-fn main() {
-    prisma_client_rust_cli::run();
-}
