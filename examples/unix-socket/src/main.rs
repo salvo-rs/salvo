12ef829e2ff0df3c27f07457604c6f451d820db4
Chrislearn Young <chris@acroidea.com>
2026-03-31 23:17:04 +0800

    fix: refine Unix feature checks for main function in example
    Signed-off-by: Chrislearn Young <chris@acroidea.com>

--- examples/unix-socket/src/main.rs
+++ examples/unix-socket/src/main.rs
@@ -1,4 +1,4 @@
-#[cfg(unix)]
+#[cfg(all(feature = "unix", unix))]
 #[tokio::main]
 async fn main() {
     use salvo::prelude::*;
@@ -8,7 +8,7 @@ async fn main() {
     Server::new(acceptor).serve(router).await;
 }
 
-#[cfg(not(unix))]
+#[cfg(not(all(feature = "unix", unix)))]
 #[tokio::main]
 async fn main() {
     println!("This example requires the 'unix' feature and must be run on a Unix system (Linux, macOS, BSD)");
